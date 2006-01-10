1b21130e879de459f00ecfd6919474f6506f0b53
Tobias Koenig <tokoe@kde.org>
2006-01-10 22:11:32 +0000

    Add initial version of libakonadi
    CCMAIL: kde-pim@kde.org
    
    svn path=/trunk/KDE/kdepim/libakonadi/; revision=496626

---
+++ job.cpp
@@ -0,0 +1,121 @@
+/*
+    This file is part of libakonadi.
+
+    Copyright (c) 2006 Tobias Koenig <tokoe@kde.org>
+                  2006 Marc Mutz <mutz@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include <QEventLoop>
+
+#include "job.h"
+
+using namespace PIM;
+
+DataReference::DataReference()
+{
+}
+
+DataReference::DataReference( const QString &persistanceID, const QString &externalUrl )
+  : mPersistanceID( persistanceID ), mExternalUrl( externalUrl )
+{
+}
+
+DataReference::~DataReference()
+{
+}
+
+QString DataReference::persistanceID() const
+{
+  return mPersistanceID;
+}
+
+QUrl DataReference::externalUrl() const
+{
+  return mExternalUrl;
+}
+
+
+class Job::JobPrivate
+{
+  public:
+    int mError;
+};
+
+Job::Job( QObject *parent )
+  : QObject( parent ), d( new JobPrivate )
+{
+  d->mError = 0;
+}
+
+Job::~Job()
+{
+  delete d;
+  d = 0;
+}
+
+bool Job::exec()
+{
+  QEventLoop loop( this );
+  connect( this, SIGNAL( done() ), &loop, SLOT( quit() ) );
+  doStart();
+  loop.exec();
+
+  return ( d->mError == 0 );
+}
+
+void Job::start()
+{
+  doStart();
+}
+
+void Job::kill()
+{
+  d->mError = UserCanceled;
+  emit done( this );
+}
+
+int Job::error() const
+{
+  return d->mError;
+}
+
+QString Job::errorText() const
+{
+  switch ( d->mError ) {
+    case None:
+      return QString();
+      break;
+    case ConnectionFailed:
+      return tr( "Can't connect to pim storage service." );
+      break;
+    case UserCanceled:
+      return tr( "User canceled transmission." );
+      break;
+    case Unknown:
+    default:
+      return tr( "Unknown Error." );
+      break;
+  }
+}
+
+void Job::setError( int error )
+{
+  d->mError = error;
+}
+
+#include "job.moc"
---
+++ job.h
@@ -0,0 +1,191 @@
+/*
+    This file is part of libakonadi.
+
+    Copyright (c) 2006 Tobias Koenig <tokoe@kde.org>
+                  2006 Marc Mutz <mutz@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef PIM_JOB_H
+#define PIM_JOB_H
+
+#include <QObject>
+#include <QString>
+#include <QUrl>
+
+namespace PIM {
+
+/**
+  This class encapsulates a reference to a pim object in
+  the pim storage service.
+ */
+class DataReference
+{
+  public:
+    typedef QList<DataReference> List;
+
+    /**
+      Creates an empty DataReference.
+     */
+    DataReference();
+
+    /**
+      Creates a new DataReference with the given persistanceID and externalUrl.
+     */
+    DataReference( const QString &persistanceID, const QString &externalUrl );
+
+    /**
+      Destroys the DataReference.
+     */
+    ~DataReference();
+
+    /**
+      Returns the persistance id of the DataReference or an empty string if no
+      id is set.
+     */
+    QString persistanceID() const;
+
+    /**
+      Returns the external url of the DataReference or an empty string if no
+      url is set.
+     */
+    QUrl externalUrl() const;
+
+  private:
+    QString mPersistanceID;
+    QString mExternalUrl;
+
+    class DataReferencePrivate;
+    DataReferencePrivate *d;
+};
+
+/**
+  This class encapsulates a request to the pim storage service,
+  the code looks like
+
+  \code
+    PIM::Job *job = new PIM::SomeJob( some parameter );
+    connect( job, SIGNAL( done( PIM::Job* ) ),
+             this, SLOT( slotResult( PIM::Job* ) ) );
+    job->start();
+  \endcode
+
+  And the slotResult is usually at least:
+
+  \code
+    if ( job->error() )
+      // handle error...
+  \endcode
+
+  With the synchronous interface the code looks like
+
+  \code
+    PIM::SomeJob job( some parameter );
+    if ( !job.exec() ) {
+      qDebug( "Error: %s", qPrintable( job.errorString() ) );
+    } else {
+      // do something
+    }
+  \endcode
+
+  Subclasses must reimplement @see doStart().
+ */
+class Job : public QObject
+{
+  Q_OBJECT
+
+  public:
+
+    /**
+      Error codes that can be emitted by this class,
+      subclasses can provide additional codes.
+     */
+    enum Error
+    {
+      None,
+      ConnectionFailed,
+      UserCanceled,
+      Unknown
+    };
+
+    /**
+      Creates a new job.
+     */
+    Job( QObject *parent = 0 );
+
+    /**
+      Destroys the job.
+     */
+    virtual ~Job();
+
+    /**
+      Executes the job synchronous.
+     */
+    bool exec();
+
+    /**
+      Starts the job asynchronous. When the job finished successfull,
+      @see done() is emitted.
+     */
+    void start();
+
+    /**
+      Abort this job.
+
+      The @see done() signal is emitted nonetheless, but @see error()
+      returns a special error code.
+     */
+    virtual void kill();
+
+    /**
+      Returns the error code, if there has been an error, 0 otherwise.
+     */
+    int error() const;
+
+    /**
+      Returns the error string, if there has been an error, an empty
+      string otherwise.
+     */
+    virtual QString errorText() const;
+
+  signals:
+    /**
+      This signal is emitted when the started job finished.
+
+      @param job A pointer to the job which emitted the signal.
+     */
+    void done( PIM::Job *job );
+
+  protected:
+    /**
+      Subclasses have to use this method to set an error code.
+     */
+    void setError( int );
+
+  private:
+    /**
+      This method must be reimplemented in the concrete jobs.
+     */
+    virtual void doStart() = 0;
+
+    class JobPrivate;
+    JobPrivate *d;
+};
+
+}
+
+#endif
