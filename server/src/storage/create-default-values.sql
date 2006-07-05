0375da3a90688501feedab16063dd090daf22ebb
Cornelius Schumacher <schumacher@kde.org>
2006-07-05 18:31:06 +0000

    iCalendar is text/calendar.
    svn path=/trunk/KDE/kdepim/akonadiserver/; revision=558634

--- server/src/storage/create-default-values.sql
+++ server/src/storage/create-default-values.sql
@@ -3,14 +3,14 @@ INSERT INTO CachePolicies (name) VALUES ('temporary');
 INSERT INTO CachePolicies (name) VALUES ('permanent');
 
 INSERT INTO MimeTypes (mime_type) VALUES ('message/rfc822');
-INSERT INTO MimeTypes (mime_type) VALUES ('text/ical');
+INSERT INTO MimeTypes (mime_type) VALUES ('text/calendar');
 INSERT INTO MimeTypes (mime_type) VALUES ('text/vcard');
 INSERT INTO MimeTypes (mime_type) VALUES ('directory/inode');
 
 INSERT INTO MetaTypes SELECT NULL, 'from', id FROM MimeTypes WHERE mime_type = 'message/rfc822';
 INSERT INTO MetaTypes SELECT NULL, 'to', id FROM MimeTypes WHERE mime_type = 'message/rfc822';
-INSERT INTO MetaTypes SELECT NULL, 'start_date', id FROM MimeTypes WHERE mime_type = 'text/ical';
-INSERT INTO MetaTypes SELECT NULL, 'end_date', id FROM MimeTypes WHERE mime_type = 'text/ical';
+INSERT INTO MetaTypes SELECT NULL, 'start_date', id FROM MimeTypes WHERE mime_type = 'text/calendar';
+INSERT INTO MetaTypes SELECT NULL, 'end_date', id FROM MimeTypes WHERE mime_type = 'text/calendar';
 INSERT INTO MetaTypes SELECT NULL, 'email', id FROM MimeTypes WHERE mime_type = 'text/vcard';
 
 INSERT INTO Flags (name) VALUES ('important');
