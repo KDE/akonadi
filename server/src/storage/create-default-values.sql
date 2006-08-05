6a8941922cc496056d5730167cc8ac27398fa682
Volker Krause <vkrause@kde.org>
2006-08-05 14:36:42 +0000

    Make is usable for the unit tests again.
    svn path=/trunk/KDE/kdepim/akonadi/server/; revision=570027

--- server/src/storage/create-default-values.sql
+++ server/src/storage/create-default-values.sql
@@ -1,18 +1,22 @@
+DELETE FROM CachePolicies;
 INSERT INTO CachePolicies (name) VALUES ('none');
 INSERT INTO CachePolicies (name) VALUES ('temporary');
 INSERT INTO CachePolicies (name) VALUES ('permanent');
 
+DELETE FROM MimeTypes;
 INSERT INTO MimeTypes (id,mime_type) VALUES (0,'message/rfc822');
 INSERT INTO MimeTypes (id,mime_type) VALUES (1,'text/calendar');
 INSERT INTO MimeTypes (id,mime_type) VALUES (2,'text/vcard');
 INSERT INTO MimeTypes (id,mime_type) VALUES (3,'inode/directory');
 
+DELETE FROM MetaTypes;
 INSERT INTO MetaTypes SELECT NULL, 'from', id FROM MimeTypes WHERE mime_type = 'message/rfc822';
 INSERT INTO MetaTypes SELECT NULL, 'to', id FROM MimeTypes WHERE mime_type = 'message/rfc822';
 INSERT INTO MetaTypes SELECT NULL, 'start_date', id FROM MimeTypes WHERE mime_type = 'text/calendar';
 INSERT INTO MetaTypes SELECT NULL, 'end_date', id FROM MimeTypes WHERE mime_type = 'text/calendar';
 INSERT INTO MetaTypes SELECT NULL, 'email', id FROM MimeTypes WHERE mime_type = 'text/vcard';
 
+DELETE FROM Flags;
 INSERT INTO Flags (name) VALUES ('important');
 INSERT INTO Flags (name) VALUES ('has_attachment');
 INSERT INTO Flags (name) VALUES ('spam');
@@ -22,20 +26,24 @@ INSERT INTO Flags (name) VALUES ('\Deleted');
 INSERT INTO Flags (name) VALUES ('\Seen');
 INSERT INTO Flags (name) VALUES ('\Draft');
 
+DELETE FROM Locations;
 INSERT INTO Locations (id, uri, cachepolicy_id, resource_id, exists_count, recent_count, unseen_count, first_unseen, uid_validity) VALUES (0, '/foo', 0, 0, 3, 0, 0, 0, 0);
 INSERT INTO Locations (id, uri, cachepolicy_id, resource_id, exists_count, recent_count, unseen_count, first_unseen, uid_validity) VALUES (1, '/foo/bar', 0, 0, 2, 0, 0, 0, 0);
 INSERT INTO Locations (id, uri, cachepolicy_id, resource_id, exists_count, recent_count, unseen_count, first_unseen, uid_validity) VALUES (2, '/foo/bar/bla', 0, 0, 2, 0, 0, 0, 0);
 INSERT INTO Locations (id, uri, cachepolicy_id, resource_id, exists_count, recent_count, unseen_count, first_unseen, uid_validity) VALUES (3, '/foo/bla', 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO Locations (id, uri, cachepolicy_id, resource_id, exists_count, recent_count, unseen_count, first_unseen, uid_validity) VALUES (4, '/foo2', 0, 1, 2, 3, 4, 5, 6);
 
+DELETE FROM Resources;
 INSERT INTO Resources (id, name, cachepolicy_id) VALUES(0, 'res1', 0);
 INSERT INTO Resources (id, name, cachepolicy_id) VALUES(1, 'res2', 0);
 INSERT INTO Resources (id, name, cachepolicy_id) VALUES(2, 'res3', 0);
 
+DELETE FROM PersistentSearches;
 INSERT INTO PersistentSearches (id, name, query) VALUES(0, 'kde-core-devel', 'MIMETYPE message/rfc822 HEADER From kde-core-devel@kde.org');
 INSERT INTO PersistentSearches (id, name, query) VALUES(1, 'all', 'MIMETYPE message/rfc822 ALL');
 INSERT INTO PersistentSearches (id, name, query) VALUES(2, 'Test �er', 'MIMETYPE message/rfc822 BODY "Test �er"');
 
+DELETE FROM LocationMimeTypes;
 INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 0, 1);
 INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 0, 2);
 INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 1, 2);
@@ -44,6 +52,7 @@ INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 1, 3);
 INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 2, 3);
 INSERT INTO LocationMimeTypes ( location_id, mimetype_id) VALUES( 3, 3);
 
+DELETE FROM PimItems;
 INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (0, 'A', 'testmailbody', 0, 0);
 INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (1, 'B', 'testmailbody1', 0, 0);
 INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (2, 'C', 'testmailbody2', 0, 0);
@@ -60,11 +69,13 @@ INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (12,
 INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (13, 'N', 'testmailbody13', 0, 0);
 INSERT INTO PimItems (id, remote_id, data, location_id, mimetype_id) VALUES (14, 'O', 'testmailbody14', 0, 0);
 
+DELETE FROM ItemFlags;
 INSERT INTO ItemFlags (flag_id, pimitem_id) VALUES (4, 0);
 INSERT INTO ItemFlags (flag_id, pimitem_id) VALUES (5, 0);
 INSERT INTO ItemFlags (flag_id, pimitem_id) VALUES (6, 0);
 INSERT INTO ItemFlags (flag_id, pimitem_id) VALUES (4, 1);
 INSERT INTO ItemFlags (flag_id, pimitem_id) VALUES (4, 1);
 
+DELETE FROM ItemMetaData;
 INSERT INTO ItemMetaData (id, data, metatype_id, pimitem_id) VALUES (0, "testmetatata", 0, 0);
 INSERT INTO ItemMetaData (id, data, metatype_id, pimitem_id) VALUES (1, "testmetatata2", 0, 3);
--- server/src/storage/make-new-db.sh
+++ server/src/storage/make-new-db.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 akonadidb=$HOME/.akonadi/akonadi.db
 
-rm $akonadidb
-cat create-database-schema.sql | sqlite3 $akonadidb
 cat create-default-values.sql | sqlite3 $akonadidb
