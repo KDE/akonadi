edc203e7881033d3df0f654a75cc96cca508ae04
Till Adam <adam@kde.org>
2007-04-29 08:47:41 +0000

    Claim a bit of copyright.
    svn path=/trunk/KDE/kdepim/akonadi/libakonadi/; revision=659021

--- item.h
+++ item.h
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2006 Volker Krause <vkrause@kde.org>
+                  2007 Till Adam <adam@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
