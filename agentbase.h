a8dd3345d179065cd309760c7ad49d7a3a36ac74
Allen Winter <winter@kde.org>
2008-03-27 17:38:08 +0000

    tell Krazy not to worry about a dpointer inside the AgentBase::Observer class.
    svn path=/trunk/KDE/kdepimlibs/; revision=790861

--- agentbase.h
+++ agentbase.h
@@ -144,7 +144,7 @@ class AKONADI_EXPORT AgentBase : public QObject, protected QDBusContext
      * }
      * @endcode
      */
-    class AKONADI_EXPORT Observer
+    class AKONADI_EXPORT Observer  // krazy:exclude=dpointer
     {
       public:
         /**
