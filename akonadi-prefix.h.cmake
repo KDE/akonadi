42d73f3afd269b48e7fcdf07654982dcbfa11cb0
Will Stephenson <wstephenson@kde.org>
2008-04-23 17:25:54 +0000

    Move Akonadi server and shared components to kdesupport.
    svn path=/trunk/KDE/kdepimlibs/; revision=800213

--- akonadi-prefix.h.cmake
+++
@@ -1,6 +0,0 @@
-/* This file contains all the paths that change when changing the installation prefix */
-
-#define AKONADIPREFIX "${CMAKE_INSTALL_PREFIX}"
-#define AKONADIDATA   "${SHARE_INSTALL_PREFIX}"
-#define AKONADICONFIG "${CONFIG_INSTALL_DIR}"
-
