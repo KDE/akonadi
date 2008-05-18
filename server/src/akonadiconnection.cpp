2e413ae625118d92c3bcae965c6be9cf93d20dab
Volker Krause <vkrause@kde.org>
2008-05-18 11:31:36 +0000

    Check if the server protocol version is recent enough.
    svn path=/trunk/kdesupport/akonadi/; revision=809101

--- server/src/akonadiconnection.cpp
+++ server/src/akonadiconnection.cpp
@@ -88,7 +88,7 @@ void AkonadiConnection::run()
     connect( m_socket, SIGNAL( disconnected() ),
              this, SLOT( slotDisconnected() ), Qt::DirectConnection );
 
-    writeOut( "* OK Akonadi Almost IMAP Server");
+    writeOut( "* OK Akonadi Almost IMAP Server [PROTOCOL 1]");
 
     exec();
     delete m_socket;
