4a61f22f81e39880081352b4bd30dd0e6a552c95
Volker Krause <vkrause@kde.org>
2009-01-25 15:35:17 +0000

    - move the testrunner environment for the libakonadi unittests - add a replacement for QTEST_KDEMAIN that does not change KDEHOME and   XDG_* environment variables (the testrunner takes care of that   already) - add a test for the testrunner environment setup
    svn path=/trunk/KDE/kdepimlibs/; revision=916580

---
+++ qtest_akonadi.h
@@ -0,0 +1,56 @@
+/* This file is based on qtest_kde.h from kdelibs
+    Copyright (C) 2006 David Faure <faure@kde.org>
+    Copyright (C) 2009 Volker Krause <vkrause@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef QTEST_AKONADI_H
+#define QTEST_AKONADI_H
+
+#include <qtest_kde.h>
+
+/**
+* \short Akonadi Replacement for QTEST_MAIN from QTestLib
+*
+* This macro should be used for classes that run inside the Akonadi Testrunner.
+* So instead of writing QTEST_MAIN( TestClass ) you write
+* QTEST_AKONADIMAIN( TestClass, GUI ).
+*
+* \param TestObject The class you use for testing.
+* \param flags one of KDEMainFlag. This is passed to the QApplication constructor.
+*
+* \see KDEMainFlag
+* \see QTestLib
+* \see QTEST_KDEMAIN
+*/
+#define QTEST_AKONADIMAIN(TestObject, flags) \
+int main(int argc, char *argv[]) \
+{ \
+  setenv("LC_ALL", "C", 1); \
+  unsetenv("KDE_COLOR_DEBUG"); \
+  KAboutData aboutData( QByteArray("qttest"), QByteArray(), ki18n("KDE Test Program"), QByteArray("version") );  \
+  KDEMainFlags mainFlags = flags;                         \
+  KComponentData cData(&aboutData); \
+  QApplication app( argc, argv, (mainFlags & GUI) != 0 ); \
+  app.setApplicationName( "qttest" ); \
+  qRegisterMetaType<KUrl>(); /*as done by kapplication*/ \
+  qRegisterMetaType<KUrl::List>(); \
+  TestObject tc; \
+  KGlobal::ref(); /* don't quit qeventloop after closing a mainwindow */ \
+  return QTest::qExec( &tc, argc, argv ); \
+}
+
+#endif
---
+++ tests/testenvironmenttest.cpp
@@ -0,0 +1,66 @@
+/*
+    Copyright (c) 2009 Volker Krause <vkrause@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include <akonadi/qtest_akonadi.h>
+#include <akonadi/servermanager.h>
+
+#include <QDir>
+#include <QObject>
+#include <QDBusConnection>
+#include <QDBusConnectionInterface>
+
+using namespace Akonadi;
+
+/**
+  This test verifies that the testrunner set everything up correctly, so all the
+  other tests work as expected.
+*/
+class TestEnvironmentTest : public QObject
+{
+  Q_OBJECT
+  private slots:
+    void testEnvironment()
+    {
+      QVERIFY( qgetenv( "KDEHOME" ).startsWith( QDir::tempPath().toLocal8Bit() ) );
+    }
+
+    void testDBus()
+    {
+      QVERIFY( QDBusConnection::sessionBus().isConnected() );
+    }
+
+    void testAkonadiServer()
+    {
+      QVERIFY( ServerManager::isRunning() );
+    }
+
+    void testResources()
+    {
+      QVERIFY( QDBusConnection::sessionBus().interface()->isServiceRegistered(
+        "org.freedesktop.Akonadi.Agent.akonadi_knut_resource_0" ) );
+      QVERIFY( QDBusConnection::sessionBus().interface()->isServiceRegistered(
+        "org.freedesktop.Akonadi.Agent.akonadi_knut_resource_1" ) );
+      QVERIFY( QDBusConnection::sessionBus().interface()->isServiceRegistered(
+        "org.freedesktop.Akonadi.Agent.akonadi_knut_resource_2" ) );
+    }
+};
+
+QTEST_AKONADIMAIN( TestEnvironmentTest, NoGUI )
+
+#include "testenvironmenttest.moc"
