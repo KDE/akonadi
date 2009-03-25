d4f586dd87da8cbf5a1be81335aacc8cc18ddcbf
Benjamin Reed <ranger@befunk.com>
2009-03-25 04:53:33 +0000

    forward-port various Mac OS X fixes from the 4.2 branch to trunk
    svn path=/trunk/KDE/kdepimlibs/; revision=944123

--- tests/testrunner/setup.cpp
+++ tests/testrunner/setup.cpp
@@ -37,6 +37,10 @@
 #include <signal.h>
 #include <unistd.h>
 
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#endif
+
 QMap<QString, QString> SetupTest::environment() const
 {
   QMap<QString, QString> env;
@@ -58,9 +62,15 @@ bool SetupTest::clearEnvironment()
 
   foreach ( const QString& key, keys ) {
     if ( key != QLatin1String( "HOME" ) ) {
+// work around a bug in the Mac OS X 10.4.0 SDK
+#if defined(MAC_OS_X_VERSION_MAX_ALLOWED) && (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4)
+      /* on OSX 10.4, unsetenv is a void, not a boolean */
+      unsetenv( key.toAscii() );
+#else
       if ( !unsetenv( key.toAscii() ) ) {
         return false;
       }
+#endif
     }
   }
 
