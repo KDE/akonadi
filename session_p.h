f1969c85efe631053c217741343fb7d6016971f7
Volker Krause <vkrause@kde.org>
2009-06-17 12:22:08 +0000

    Restore API symmetry to the Item jobs by adding an explicit CollectionMoveJob instead of abusing CollectionModifyJob as discussed with Steve some time ago. Since collection moving didn't work until recently (and thus especially not in 4.2), this does not break compatibility.
    svn path=/trunk/KDE/kdepimlibs/; revision=982975

--- session_p.h
+++ session_p.h
@@ -90,7 +90,7 @@ class SessionPrivate
     */
     void writeData( const QByteArray &data );
 
-    static int minimumProtocolVersion() { return 13; }
+    static int minimumProtocolVersion() { return 14; }
 
     Session *mParent;
     QByteArray sessionId;
