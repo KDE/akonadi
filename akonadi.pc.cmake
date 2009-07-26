d59db80aef6068ccd647ebd79805c268ee987dac
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2009-07-26 12:28:19 +0000

    pkgconfig will work much better if we don't use AKONADI_VERSION_STRING
    svn path=/trunk/kdesupport/akonadi/; revision=1002569

--- akonadi.pc.cmake
+++ akonadi.pc.cmake
@@ -5,7 +5,7 @@ includedir=@CMAKE_INSTALL_PREFIX@/include
 
 Name: Akonadi
 Description: Akonadi server and infrastructure needed to build client libraries and applications
-Version: @AKONADI_VERSION_STRING@
+Version: @AKONADI_VERSION@
 Requires: QtCore QtSql QtDBus
 Libs: -L${libdir} -lakonadiprotocolinternals
 Cflags: -I${includedir}
