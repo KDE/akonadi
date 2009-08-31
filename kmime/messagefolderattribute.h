4818c87b5b3a9c473265c972a20330403eab76b1
Volker Krause <vkrause@kde.org>
2009-08-31 09:01:36 +0000

    SVN_MERGE
    Merged revisions 1009646 via svnmerge from 
    https://vkrause@svn.kde.org/home/kde/branches/work/akonadi-ports/kdepimlibs
    
    ........
      r1009646 | ervin | 2009-08-10 17:03:28 +0200 (Mon, 10 Aug 2009) | 4 lines
      
      Add a new attribute to store display settings for message folders. First
      one (and only one for now) is to determine if a folder has to be
      considered as a folder containing outbound messages or not.
    ........
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1017694

---
+++ kmime/messagefolderattribute.h
@@ -0,0 +1,78 @@
+/*
+    Copyright (c) 2009 Kevin Ottens <ervin@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef AKONADI_MESSAGEFOLDERATTRIBUTE_H
+#define AKONADI_MESSAGEFOLDERATTRIBUTE_H
+
+#include "akonadi-kmime_export.h"
+#include <akonadi/attribute.h>
+
+#include <QtCore/QByteArray>
+
+namespace Akonadi {
+
+/**
+  Message folder information. Used eg. by mail clients to decide how to display the content of such collections
+*/
+class AKONADI_KMIME_EXPORT MessageFolderAttribute : public Attribute
+{
+  public:
+    /**
+      Creates an empty folder attribute.
+    */
+    MessageFolderAttribute();
+
+    /**
+      Copy constructor.
+    */
+    MessageFolderAttribute( const MessageFolderAttribute &other );
+
+    /**
+      Destructor.
+    */
+    ~MessageFolderAttribute();
+
+    /**
+      Indicates if the folder is supposed to contain mostly outbound messages.
+      In such a case mail clients display the recipient address, otherwise they
+      display the sender address.
+
+      @return true if the folder contains outbound messages
+    */
+    bool isOutboundFolder() const;
+
+    /**
+      Set if the folder should be considered as containing mostly outbound messages.
+     */
+    void setOutboundFolder(bool outbound);
+
+    // reimpl.
+    QByteArray type() const;
+    MessageFolderAttribute* clone() const;
+    QByteArray serialized() const;
+    void deserialize( const QByteArray &data );
+
+  private:
+    class Private;
+    Private * const d;
+};
+
+}
+
+#endif
