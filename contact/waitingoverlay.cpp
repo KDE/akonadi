37a2de766defc2299b3f9d6761bbae206ea6f1f7
Tobias Koenig <tokoe@kde.org>
2009-09-02 07:20:32 +0000

    Relicense my code under LGPL
    svn path=/trunk/KDE/kdepimlibs/; revision=1018774

--- contact/waitingoverlay.cpp
+++ contact/waitingoverlay.cpp
@@ -1,4 +1,6 @@
 /*
+    This file is part of Akonadi Contact.
+
     Copyright (c) 2008 Volker Krause <vkrause@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
