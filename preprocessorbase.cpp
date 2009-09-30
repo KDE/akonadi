cead6192a5113e5dc7d03be3481556d44d4a421f
Tobias Koenig <tokoe@kde.org>
2009-09-30 17:42:26 +0000

    compile++
    svn path=/trunk/KDE/kdepimlibs/; revision=1029773

--- preprocessorbase.cpp
+++ preprocessorbase.cpp
@@ -68,12 +68,13 @@ PreprocessorBase::~PreprocessorBase()
 {
 }
 
-void PreprocessorBase::terminateProcessing( ProcessingResult )
+void PreprocessorBase::terminateProcessing( ProcessingResult result )
 {
   Q_D( PreprocessorBase );
 
   Q_ASSERT_X( result != ProcessingDelayed, "PreprocessorBase::terminateProcessing", "You should never pass ProcessingDelayed to this function" );
   Q_ASSERT_X( d->mInDelayedProcessing, "PreprocessorBase::terminateProcessing", "terminateProcessing() called while not in delayed processing mode" );
+  Q_UNUSED( result );
 
   d->mInDelayedProcessing = false;
   emit itemProcessed( d->mDelayedProcessingItemId );
