001531725f163e7484345ad560ce7f47f4898baa
Kevin Ottens <ervin@kde.org>
2009-10-17 21:36:14 +0000

    Merge StatisticsToolTipProxyModel into StatisticsProxyModel.
    svn path=/trunk/KDE/kdepimlibs/; revision=1036830

--- statisticsproxymodel.cpp
+++ statisticsproxymodel.cpp
@@ -23,6 +23,7 @@
 #include "entitytreemodel.h"
 #include "collectionutils_p.h"
 
+#include <akonadi/collectionquotaattribute.h>
 #include <akonadi/collectionstatistics.h>
 #include <akonadi/entitydisplayattribute.h>
 
@@ -43,11 +44,105 @@ class StatisticsProxyModel::Private
 {
   public:
     Private( StatisticsProxyModel *parent )
-      : mParent( parent )
+      : mParent( parent ), mToolTipEnabled( false ), mExtraColumnsEnabled( true )
     {
     }
 
+    int sourceColumnCount( const QModelIndex &parent )
+    {
+      return mParent->sourceModel()->columnCount( mParent->mapToSource( parent ) );
+    }
+
+    QString toolTipForCollection( const QModelIndex &index, const Collection &collection )
+    {
+      QString bckColor = QApplication::palette().color( QPalette::ToolTipBase ).name();
+      QString txtColor = QApplication::palette().color( QPalette::ToolTipText ).name();
+
+      QString tip = QString::fromLatin1(
+        "<table width=\"100%\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\">\n"
+        );
+
+      tip += QString::fromLatin1(
+        "  <tr>\n"
+        "    <td bgcolor=\"%1\" colspan=\"2\" align=\"left\" valign=\"middle\">\n"
+        "      <div style=\"color: %2; font-weight: bold;\">\n"
+        "      %3\n"
+        "      </div>\n"
+        "    </td>\n"
+        "  </tr>\n"
+        ).arg( txtColor ).arg( bckColor ).arg( index.data( Qt::DisplayRole ).toString() );
+
+
+      tip += QString::fromLatin1(
+        "  <tr>\n"
+        "    <td align=\"left\" valign=\"top\">\n"
+        );
+
+      tip += QString::fromLatin1(
+        "      <strong>%1</strong>: %2<br>\n"
+        "      <strong>%3</strong>: %4<br><br>\n"
+        ).arg( i18n("Total Messages") ).arg( collection.statistics().count() )
+         .arg( i18n("Unread Messages") ).arg( collection.statistics().unreadCount() );
+
+      if ( collection.hasAttribute<CollectionQuotaAttribute>() ) {
+        CollectionQuotaAttribute *quota = collection.attribute<CollectionQuotaAttribute>();
+        if ( quota->currentValue() > -1 && quota->maximumValue() > 0 ) {
+          qreal percentage = ( 100.0 * quota->currentValue() ) / quota->maximumValue();
+
+          if ( qAbs( percentage ) >= 0.01 ) {
+            QString percentStr = QString::number( percentage, 'f', 2 );
+            tip += QString::fromLatin1(
+              "      <strong>%1</strong>: %2%<br>\n"
+              ).arg( i18n( "Quota" ) ).arg( percentStr );
+          }
+        }
+      }
+
+      tip += QString::fromLatin1(
+        "      <strong>%1</strong>: %2<br>\n"
+        ).arg( i18n("Storage Size") ).arg( KIO::convertSize( (KIO::filesize_t)( collection.statistics().size() ) ) );
+
+
+      QString iconName = CollectionUtils::defaultIconName( collection );
+      if ( collection.hasAttribute<EntityDisplayAttribute>() &&
+           !collection.attribute<EntityDisplayAttribute>()->iconName().isEmpty() ) {
+        iconName = collection.attribute<EntityDisplayAttribute>()->iconName();
+      }
+
+      int iconSizes[] = { 32, 22 };
+      QString iconPath;
+
+      for ( int i = 0; i < 2; i++ ) {
+        iconPath = KIconLoader::global()->iconPath( iconName, -iconSizes[ i ], true );
+        if ( !iconPath.isEmpty() )
+          break;
+      }
+
+      if ( iconPath.isEmpty() ) {
+        iconPath = KIconLoader::global()->iconPath( QLatin1String("folder"), -32, false );
+      }
+
+      tip += QString::fromLatin1(
+        "    </td>\n"
+        "    <td align=\"right\" valign=\"top\">\n"
+        "      <table border=\"0\"><tr><td width=\"32\" height=\"32\" align=\"center\" valign=\"middle\">\n"
+        "      <img src=\"%1\">\n"
+        "      </td></tr></table>\n"
+        "    </td>\n"
+        "  </tr>\n"
+        ).arg( iconPath );
+
+      tip += QString::fromLatin1(
+        "</table>"
+        );
+
+      return tip;
+    }
+
     StatisticsProxyModel *mParent;
+
+    bool mToolTipEnabled;
+    bool mExtraColumnsEnabled;
 };
 
 StatisticsProxyModel::StatisticsProxyModel( QObject *parent )
@@ -61,15 +156,35 @@ StatisticsProxyModel::~StatisticsProxyModel()
   delete d;
 }
 
+void StatisticsProxyModel::setToolTipEnabled( bool enable )
+{
+  d->mToolTipEnabled = enable;
+}
+
+bool StatisticsProxyModel::isToolTipEnabled() const
+{
+  return d->mToolTipEnabled;
+}
+
+void StatisticsProxyModel::setExtraColumnsEnabled( bool enable )
+{
+  d->mExtraColumnsEnabled = enable;
+}
+
+bool StatisticsProxyModel::isExtraColumnsEnabled() const
+{
+  return d->mExtraColumnsEnabled;
+}
+
 QModelIndex Akonadi::StatisticsProxyModel::index( int row, int column, const QModelIndex & parent ) const
 {
     if (!hasIndex(row, column, parent))
       return QModelIndex();
 
-    
+
     int sourceColumn = column;
 
-    if ( column>=columnCount( parent)-3 ) {
+    if ( column>=d->sourceColumnCount( parent ) ) {
       sourceColumn = 0;
     }
 
@@ -79,16 +194,16 @@ QModelIndex Akonadi::StatisticsProxyModel::index( int row, int column, const QMo
 
 QVariant StatisticsProxyModel::data( const QModelIndex & index, int role) const
 {
-  if ( role == Qt::DisplayRole && index.column()>=columnCount( index.parent() )-3 ) {
+  if ( role == Qt::DisplayRole && index.column()>=d->sourceColumnCount( index.parent() ) ) {
     const QModelIndex sourceIndex = mapToSource( index.sibling( index.row(), 0 ) );
     Collection collection = sourceModel()->data( sourceIndex, EntityTreeModel::CollectionRole ).value<Collection>();
 
     if ( collection.isValid() && collection.statistics().count()>=0 ) {
-      if ( index.column() == columnCount( QModelIndex() )-1 ) {
+      if ( index.column() == d->sourceColumnCount( QModelIndex() )+2 ) {
         return KIO::convertSize( (KIO::filesize_t)( collection.statistics().size() ) );
-      } else if ( index.column() == columnCount( QModelIndex() )-2 ) {
+      } else if ( index.column() == d->sourceColumnCount( QModelIndex() )+1 ) {
         return collection.statistics().count();
-      } else if ( index.column() == columnCount( QModelIndex() )-3 ) {
+      } else if ( index.column() == d->sourceColumnCount( QModelIndex() ) ) {
         if ( collection.statistics().unreadCount() > 0 ) {
           return collection.statistics().unreadCount();
         } else {
@@ -99,8 +214,18 @@ QVariant StatisticsProxyModel::data( const QModelIndex & index, int role) const
         return QVariant();
       }
     }
-  } else if ( role == Qt::TextAlignmentRole && index.column()>=columnCount( index.parent() )-3 ) {
+  } else if ( role == Qt::TextAlignmentRole && index.column()>=d->sourceColumnCount( index.parent() ) ) {
     return Qt::AlignRight;
+
+  } else if ( role == Qt::ToolTipRole && d->mToolTipEnabled ) {
+    const QModelIndex sourceIndex = mapToSource( index.sibling( index.row(), 0 ) );
+    Collection collection
+      = sourceModel()->data( sourceIndex,
+                             EntityTreeModel::CollectionRole ).value<Collection>();
+
+    if ( collection.isValid() && collection.statistics().count()>0 ) {
+      return d->toolTipForCollection( index, collection );
+    }
   }
 
   return QAbstractProxyModel::data( index, role );
@@ -109,11 +234,11 @@ QVariant StatisticsProxyModel::data( const QModelIndex & index, int role) const
 QVariant StatisticsProxyModel::headerData( int section, Qt::Orientation orientation, int role) const
 {
   if ( orientation == Qt::Horizontal && role == Qt::DisplayRole ) {
-    if ( section == columnCount( QModelIndex() )-1 ) {
+    if ( section == d->sourceColumnCount( QModelIndex() )+2 ) {
       return i18n( "Size" );
-    } else if ( section == columnCount( QModelIndex() )-2 ) {
+    } else if ( section == d->sourceColumnCount( QModelIndex() )+1 ) {
       return i18n( "Total" );
-    } else if ( section == columnCount( QModelIndex() )-3 ) {
+    } else if ( section == d->sourceColumnCount( QModelIndex() ) ) {
       return i18n( "Unread" );
     }
   }
@@ -123,7 +248,7 @@ QVariant StatisticsProxyModel::headerData( int section, Qt::Orientation orientat
 
 Qt::ItemFlags StatisticsProxyModel::flags( const QModelIndex & index ) const
 {
-  if ( index.column()>=columnCount( index.parent() )-3 ) {
+  if ( index.column()>=d->sourceColumnCount( index.parent() ) ) {
     return QSortFilterProxyModel::flags( index.sibling( index.row(), 0 ) )
          & ( Qt::ItemIsSelectable | Qt::ItemIsDragEnabled // Allowed flags
            | Qt::ItemIsDropEnabled | Qt::ItemIsEnabled );
@@ -134,7 +259,12 @@ Qt::ItemFlags StatisticsProxyModel::flags( const QModelIndex & index ) const
 
 int StatisticsProxyModel::columnCount( const QModelIndex & parent ) const
 {
-  return sourceModel()->columnCount( mapToSource( parent ) ) + 3;
+  if ( sourceModel()==0 ) {
+    return 0;
+  } else {
+    return d->sourceColumnCount( parent )
+         + ( d->mExtraColumnsEnabled ? 3 : 0 );
+  }
 }
 
 bool StatisticsProxyModel::dropMimeData( const QMimeData * data, Qt::DropAction action, int row, int column, const QModelIndex & parent )
