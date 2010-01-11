49c8ede29d4e4b619524eb187214715298971c45
Volker Krause <vkrause@kde.org>
2010-01-11 11:03:11 +0000

    SVN_MERGE
    Merged revisions 1056629 via svnmerge from 
    https://vkrause@svn.kde.org/home/kde/branches/work/akonadi-ports/kdepimlibs
    
    ........
      r1056629 | vkrause | 2009-11-30 14:41:32 +0100 (Mon, 30 Nov 2009) | 2 lines
      
      Client side interface for search support in agents.
    ........
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1072987

---
+++ agentsearchinterface_p.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (c) 2009 Volker Krause <vkrause@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef AKOANDI_AGENTSEARCHINTERFACE_P_H
+#define AKOANDI_AGENTSEARCHINTERFACE_P_H
+
+#include "agentsearchinterface.h"
+
+#include <QObject>
+
+namespace Akonadi {
+
+class AgentSearchInterfacePrivate : public QObject
+{
+  Q_OBJECT
+  public:
+    AgentSearchInterfacePrivate( AgentSearchInterface* qq );
+
+    void addSearch( const QString &query, const QString &queryLanguage, quint64 resultCollectionId );
+    void removeSearch( quint64 resultCollectionId );
+
+  private:
+    AgentSearchInterface* q;
+};
+
+}
+
+#endif
