e48a5a6c73c292cd37a8848fde0d42685685f38e
Burkhard Lück <lueck@hube-lueck.de>
2010-01-26 19:23:20 +0000

    load translation catalogs
    svn path=/trunk/KDE/kdepimlibs/; revision=1080707

--- contact/contactgroupeditordialog.cpp
+++ contact/contactgroupeditordialog.cpp
@@ -50,6 +50,7 @@ class ContactGroupEditorDialog::Private
 ContactGroupEditorDialog::ContactGroupEditorDialog( Mode mode, QWidget *parent )
   : KDialog( parent ), d( new Private( mode ) )
 {
+  KGlobal::locale()->insertCatalog( QLatin1String( "akonadicontact" ) );
   setCaption( mode == CreateMode ? i18n( "New Contact Group" ) : i18n( "Edit Contact Group" ) );
   setButtons( Ok | Cancel );
 
