5431aa6cf3c313a9a3cf7326b746fff6df310dec
Laurent Montel <montel@kde.org>
2010-06-01 08:13:23 +0000

    Add missing i18n
    svn path=/trunk/KDE/kdepimlibs/; revision=1133099

--- resourcebase.cpp
+++ resourcebase.cpp
@@ -506,7 +506,7 @@ void ResourceBasePrivate::slotPrepareItemRetrievalResult( KJob* job )
   }
   ItemFetchJob *fetch = qobject_cast<ItemFetchJob*>( job );
   if ( fetch->items().count() != 1 ) {
-    q->cancelTask( QLatin1String( "The requested item does no longer exist" ) );
+    q->cancelTask( i18n( "The requested item does no longer exist" ) );
     return;
   }
   const Item item = fetch->items().first();
