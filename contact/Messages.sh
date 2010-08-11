b2bcf3a27185fca1eb2a98f560c8455550afd5bf
Laurent Montel <montel@kde.org>
2010-08-11 17:49:31 +0000

    Fix extract messages
    svn path=/trunk/KDE/kdepimlibs/; revision=1162223

--- contact/Messages.sh
+++ contact/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC actions/*.kcfg >> rc.cpp || exit 11
-$XGETTEXT *.cpp actions/*.cpp editor/*.cpp -o $podir/akonadicontact.pot
+$XGETTEXT *.cpp actions/*.cpp editor/*.cpp editor/im/*.cpp -o $podir/akonadicontact.pot
 rm -f rc.cpp
