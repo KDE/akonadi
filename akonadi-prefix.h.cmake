55cdba9e438ac14e4fa2d9b29b659d7a4360ab04
Bjoern Ricks <bjoern.ricks@googlemail.com>
2010-09-09 18:37:56 +0000

    add possibility to change macos application bundle path
    svn path=/trunk/kdesupport/akonadi/; revision=1173568

--- akonadi-prefix.h.cmake
+++ akonadi-prefix.h.cmake
@@ -4,4 +4,4 @@
 #define AKONADIDATA   "${SHARE_INSTALL_PREFIX}"
 #define AKONADICONFIG "${CONFIG_INSTALL_DIR}"
 // the below is hardcoded to what FindKDE4Internal.cmake sets it to, since we have no way of finding that
-#define AKONADIBUNDLEPATH "/Applications/KDE4"
+#define AKONADIBUNDLEPATH "${AKONADI_BUNDLE_PATH}"
