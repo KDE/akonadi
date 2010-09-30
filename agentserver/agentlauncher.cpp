d80d98b707fe265b29d66bed7a4affdf93a50953
Bertjan Broeksema <broeksema@kde.org>
2010-09-30 15:32:18 +0000

    Add another helper application that can launch plugins in their own process (in opposite to the AgentServer that runs multiple agent plugins in the same process).
    svn path=/branches/work/akonadi-agentserver/; revision=1181283

---
+++ agentserver/agentlauncher.cpp
@@ -0,0 +1,60 @@
+/*
+    Copyright (c) 2010 Bertjan Broeksema <broeksema@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "agentpluginloader.h"
+
+#include <QtCore/QDebug>
+#include <QtCore/QStringList>
+#include <QtGui/QApplication>
+
+int main( int argc, char *argv[] )
+{
+  QApplication app( argc, argv );
+
+  if ( app.arguments().size() != 3 ) { // Expected usage: ./agent_launcher ${plugin_name} ${identifier}
+    qDebug() << "Invalid usage: expected: ./agent_launcher pluginName agentIdentifier";
+    return 1;
+  }
+
+  const QString agentPluginName = app.arguments().at( 1 );
+  const QString agentIdentifier = app.arguments().at( 2 );
+
+  AgentPluginLoader loader;
+  QPluginLoader *factory = loader.load( agentPluginName );
+  if ( factory == 0 )
+    return 1;
+
+  QObject *instance = 0;
+  const bool invokeSucceeded = QMetaObject::invokeMethod( factory->instance(),
+                                                          "createInstance",
+                                                          Qt::DirectConnection,
+                                                          Q_RETURN_ARG( QObject*, instance ),
+                                                          Q_ARG( QString, agentIdentifier ) );
+  if ( invokeSucceeded ) {
+    qDebug() << "Agent instance created in seperate process.";
+    instance->setParent( &app );
+  } else {
+    qDebug() << "Agent instance creation in seperate process failed";
+    return 2;
+  }
+
+
+  return app.exec();
+}
+
