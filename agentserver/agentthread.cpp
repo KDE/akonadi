79fc285e9f14bf6f084f506f1c36bd43e990721d
Bertjan Broeksema <broeksema@kde.org>
2010-10-15 16:21:38 +0000

    Properly ifdef for the windows case. The invokeMethod call will fail in the non-windows case otherwise because the configure( qlonglong ) only exists in AgentBase for the windows platform.
    svn path=/trunk/kdesupport/akonadi/; revision=1186286

--- agentserver/agentthread.cpp
+++ agentserver/agentthread.cpp
@@ -57,7 +57,11 @@ void AgentThread::configure( qlonglong windowId )
   QMetaObject::invokeMethod( m_instance,
                              "configure",
                              Qt::DirectConnection,
+#ifdef Q_OS_WIN
                              Q_ARG( qlonglong, windowId ) );
+#else
+                             Q_ARG( WId, windowId ) );
+#endif
 }
 
 #include "agentthread.moc"
