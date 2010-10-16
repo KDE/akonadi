e379ca5d7a522da011dbeef49a191f35051b11e0
Volker Krause <vkrause@kde.org>
2010-10-16 11:24:26 +0000

    Don't crash if threadConnection() is called after the Q(Core)Application has already been destroyed.
    svn path=/trunk/KDE/kdepimlibs/; revision=1186427

--- dbusconnectionpool.cpp
+++ dbusconnectionpool.cpp
@@ -53,7 +53,7 @@ QThreadStorage<DBusConnectionPoolPrivate *> s_perThreadConnection;
 
 QDBusConnection Akonadi::DBusConnectionPool::threadConnection()
 {
-    if ( QCoreApplication::instance()->thread() == QThread::currentThread() )
+    if ( !QCoreApplication::instance() || QCoreApplication::instance()->thread() == QThread::currentThread() )
         return QDBusConnection::sessionBus(); // main thread, use the default session bus, breaks unported resources otherwise
     if (!s_perThreadConnection.hasLocalData()) {
         s_perThreadConnection.setLocalData(new DBusConnectionPoolPrivate);
