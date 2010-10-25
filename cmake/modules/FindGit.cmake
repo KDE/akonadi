d7f97ff6c8c9446837c06b0f7a3a64f07ca51c69
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2010-10-25 12:19:51 +0200

    Refer the our local license file.

--- cmake/modules/FindGit.cmake
+++ cmake/modules/FindGit.cmake
@@ -11,7 +11,7 @@
 # Copyright 2010 Kitware, Inc.
 #
 # Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
+# see the COPYING-CMAKE-SCRIPTS file for details.
 #
 # This software is distributed WITHOUT ANY WARRANTY; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
