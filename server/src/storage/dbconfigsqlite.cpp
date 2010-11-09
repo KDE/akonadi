126f02431b0b3cf836bbc3b3c63807a955927fbe
Bjoern Ricks <bjoern.ricks@googlemail.com>
2010-11-09 12:43:14 +0100

    don't set wal and syncmode on wince. wince seems to have some problems with these queries

--- server/src/storage/dbconfigsqlite.cpp
+++ server/src/storage/dbconfigsqlite.cpp
@@ -174,6 +174,8 @@ void DbConfigSqlite::setup()
   const int sqliteVersionMajor = list[ 0 ].toInt();
   const int sqliteVersionMinor = list[ 1 ].toInt();
 
+#ifndef Q_OS_WINCE
+
   // set synchronous mode to NORMAL; see http://www.sqlite.org/pragma.html#pragma_synchronous
   if ( !query.exec( QLatin1String( "PRAGMA synchronous = 1" ) ) ) {
     akDebug() << "Could not set sqlite synchronous mode to NORMAL";
@@ -211,6 +213,8 @@ void DbConfigSqlite::setup()
 
   const QString journalMode = query.value( 0 ).toString();
   akDebug() << "sqlite journal mode is " << journalMode;
+
+#endif // Q_OS_WINCE
 
   db.close();
 
