e4a57e77e57aaf994e2150fce790b65ee5858d17
Andreas Holzammer <andy@kdab.com>
2010-11-25 13:32:52 +0100

    fix configuration of resources in agentserver for windows/wince

--- agentserver/agentthread.cpp
+++ agentserver/agentthread.cpp
@@ -59,11 +59,7 @@ void AgentThread::configure( qlonglong windowId )
   QMetaObject::invokeMethod( m_instance,
                              "configure",
                              Qt::DirectConnection,
-#ifdef Q_WS_WIN
-                             Q_ARG( qlonglong, windowId )
-#else
-                             Q_ARG( WId, windowId )
-#endif
+                             Q_ARG( WId, (WId)windowId )
                            );
 }
 
