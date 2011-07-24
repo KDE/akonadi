ce8d8665ebb349c36c65c05a107adcee8217ad8e
Christian Mollekopf <chrigi_1@fastmail.fm>
2011-07-24 20:12:34 +0200

    New noteutils namespace with the NoteMessageWrapper convenience class for akonadi notes

---
+++ notes/noteutils.cpp
@@ -0,0 +1,191 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2011 Christian Mollekopf <chrigi_1@fastmail.fm>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "noteutils.h"
+
+#include <klocalizedstring.h>
+#include <kdatetime.h>
+#include <kmime/kmime_message.h>
+#include <kdebug.h>
+
+#include <qstring.h>
+#include <qtextdocument.h>
+
+namespace Akonadi {
+namespace NoteUtils {
+
+class NoteMessageWrapper::NoteMessageWrapperPrivate
+{
+  public:
+    NoteMessageWrapperPrivate()
+    {
+    }
+
+    NoteMessageWrapperPrivate( const KMime::Message::Ptr &msg )
+    :  textFormat( Qt::PlainText )
+    {
+      if (msg.get()) {
+        title = msg->subject( true )->asUnicodeString();
+        text = msg->mainBodyPart()->decodedText( true ); //remove trailing whitespace, so we get rid of "  " in empty notes
+        from = msg->from( false )->asUnicodeString();
+        creationDate = msg->date( true )->dateTime();
+        if ( msg->contentType( false )->asUnicodeString() == "text/html" ) {
+          textFormat = Qt::RichText;
+        }
+      } else {
+        kWarning() << "Empty message";
+      }
+    }
+
+    QString title;
+    QString text;
+    QString from;
+    KDateTime creationDate;
+    Qt::TextFormat textFormat;
+};
+
+NoteMessageWrapper::NoteMessageWrapper()
+:  d_ptr( new NoteMessageWrapperPrivate() )
+{
+}
+
+NoteMessageWrapper::NoteMessageWrapper( const KMime::Message::Ptr &msg )
+:  d_ptr( new NoteMessageWrapperPrivate(msg) )
+{
+}
+
+NoteMessageWrapper::~NoteMessageWrapper()
+{
+  delete d_ptr;
+}
+
+KMime::Message::Ptr NoteMessageWrapper::message() const
+{
+  const Q_D( NoteMessageWrapper );
+  KMime::Message::Ptr msg = KMime::Message::Ptr( new KMime::Message() );
+
+  QByteArray encoding( "utf-8" );
+
+  QString title = i18nc( "The default name for new notes.", "New Note" );
+  if ( !d->title.isEmpty() ) {
+    title = d->title;
+  }
+  // Need a non-empty body part so that the serializer regards this as a valid message.
+  QString text = "  ";
+  if ( !d->text.isEmpty() ) {
+    text = d->text;
+  }
+
+  KDateTime creationDate = KDateTime::currentLocalDateTime();
+  if ( d->creationDate.isValid() ) {
+    creationDate = d->creationDate;
+  }
+
+  msg->subject( true )->fromUnicodeString( title, encoding );
+  msg->contentType( true )->setMimeType( d->textFormat == Qt::RichText ? "text/html" : "text/plain" );
+  msg->date( true )->setDateTime( creationDate );
+  msg->from( true )->fromUnicodeString( d->from, encoding );
+  msg->mainBodyPart()->fromUnicodeString( text );
+
+  msg->assemble();
+  return msg;
+}
+
+void NoteMessageWrapper::setCreationDate( const KDateTime &creationDate )
+{
+  Q_D(NoteMessageWrapper);
+  d->creationDate = creationDate;
+}
+
+KDateTime NoteMessageWrapper::creationDate() const
+{
+  const Q_D(NoteMessageWrapper);
+  return d->creationDate;
+}
+
+void NoteMessageWrapper::setFrom( const QString &from )
+{
+  Q_D(NoteMessageWrapper);
+  d->from = from;
+}
+
+QString NoteMessageWrapper::from() const
+{
+  const Q_D(NoteMessageWrapper);
+  return d->from;
+}
+
+void NoteMessageWrapper::setTitle( const QString &title )
+{
+  Q_D( NoteMessageWrapper );
+  d->title = title;
+}
+
+QString NoteMessageWrapper::title() const
+{
+  const Q_D( NoteMessageWrapper );
+  return d->title;
+}
+
+void NoteMessageWrapper::setText( const QString &text, Qt::TextFormat format )
+{
+  Q_D( NoteMessageWrapper );
+  d->text = text;
+  d->textFormat = format;
+}
+
+QString NoteMessageWrapper::text() const
+{
+  const Q_D( NoteMessageWrapper );
+  return d->text;
+}
+
+Qt::TextFormat NoteMessageWrapper::textFormat() const
+{
+  const Q_D( NoteMessageWrapper );
+  return d->textFormat;
+}
+
+QString NoteMessageWrapper::toPlainText() const
+{
+  const Q_D( NoteMessageWrapper );
+  if ( d->textFormat == Qt::PlainText ) {
+    return d->text;
+  }
+
+  //From cleanHtml in kdepimlibs/kcalutils/incidenceformatter.cpp
+  QRegExp rx( "<body[^>]*>(.*)</body>", Qt::CaseInsensitive );
+  rx.indexIn( d->text );
+  QString body = rx.cap( 1 );
+
+  return Qt::escape( body.remove( QRegExp( "<[^>]*>" ) ).trimmed() );
+}
+
+QString noteIconName()
+{
+  return QString::fromLatin1( "text-plain" );
+}
+
+QString noteMimeType()
+{
+  return QString::fromLatin1( "text/x-vnd.akonadi.note" );
+}
+
+} //End Namepsace
+} //End Namepsace
---
+++ notes/noteutils.h
@@ -0,0 +1,157 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2011 Christian Mollekopf <chrigi_1@fastmail.fm>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef NOTEUTILS_H
+#define NOTEUTILS_H
+
+#include "akonadi-notes_export.h"
+#include <qnamespace.h>
+
+class KDateTime;
+class QString;
+
+namespace boost {
+  template <typename T> class shared_ptr;
+}
+
+namespace KMime {
+  class Message;
+  typedef boost::shared_ptr<Message> MessagePtr;
+}
+namespace Akonadi {
+namespace NoteUtils {
+
+/**
+* @return mimetype for notes
+* @since 4.8
+*/
+AKONADI_NOTES_EXPORT QString noteMimeType();
+
+/**
+* @return icon for notes
+* @since 4.8
+*/
+AKONADI_NOTES_EXPORT QString noteIconName();
+
+/**
+* A convenience wrapper around KMime::Message::Ptr for notes
+*
+* This is the format used by the Akonotes Resource
+*
+* Reading a note from an Akonotes akonadi item:
+* @code
+* if ( item.hasPayload<KMime::Message::Ptr>() ) {
+*   NoteUtils::NoteMessageWrapper note(item.payload<KMime::Message::Ptr>());
+*   kDebug() << note.text();
+*   textIsRich = messageWrapper.textFormat() == Qt::RichText;
+* }
+* @endcode
+*
+* Setting the note as payload of an akonadi Item
+* @code
+* item.setMimeType(NoteUtils::noteMimeType());
+* NoteUtils::NoteMessageWrapper note;
+* note.setTitle( "title" );
+* note.setText( "text" );
+* note.setFrom( QString::fromLatin1( "MyApplication@kde4" ) );
+* item.setPayload( note.message() );
+* @endcode
+*
+* @author Christian Mollekopf <chrigi_1@fastmail.fm>
+* @since 4.8
+*/
+class AKONADI_NOTES_EXPORT NoteMessageWrapper
+{
+public:
+  NoteMessageWrapper();
+  explicit NoteMessageWrapper( const KMime::MessagePtr & );
+  ~NoteMessageWrapper();
+
+  /**
+    * Set the title of the note
+    */
+  void setTitle( const QString &title );
+
+  /**
+    * Returns the title of the note
+    */
+  QString title() const;
+
+  /**
+    * Set the text of the note
+    *
+    * @param format only Qt::PlainText and Qt::RichText is supported
+    */
+  void setText( const QString &text, Qt::TextFormat format = Qt::PlainText );
+
+  /**
+    * Returns the text of the note
+    */
+  QString text() const;
+
+  /**
+    * @return Qt::PlainText or Qt::RichText
+    */
+  Qt::TextFormat textFormat() const;
+
+  /**
+    * @return plaintext version of the text (if richtext)
+    */
+  QString toPlainText() const;
+
+  /**
+    * Set the creation date of the note (stored in the mime header)
+    */
+  void setCreationDate( const KDateTime &creationDate );
+
+  /**
+    * Returns the creation date of the note
+    */
+  KDateTime creationDate() const;
+
+  /**
+    * Set the origin (creator) of the note (stored in the mime header)
+    * This is usually the application creating the note.
+    */
+  void setFrom( const QString &from );
+
+  /**
+    * Returns the origin (creator) of the note
+    */
+  QString from() const;
+
+  /**
+    * Assemble a KMime message with the given values
+    *
+    * The message can then i.e. be stored inside an akonadi item
+    */
+  KMime::MessagePtr message() const;
+
+private:
+  //@cond PRIVATE
+  class NoteMessageWrapperPrivate;
+  NoteMessageWrapperPrivate * const d_ptr;
+  Q_DECLARE_PRIVATE( NoteMessageWrapper )
+  //@endcond
+};
+
+}
+}
+
+#endif
