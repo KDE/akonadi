0a4d31e0b914fefab226a620290d5e5e08f125c9
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-09-01 17:18:36 +0200

    compile fix

--- notes/noteutils.cpp
+++ notes/noteutils.cpp
@@ -45,7 +45,7 @@ class NoteMessageWrapper::NoteMessageWrapperPrivate
         text = msg->mainBodyPart()->decodedText( true ); //remove trailing whitespace, so we get rid of "  " in empty notes
         from = msg->from( false )->asUnicodeString();
         creationDate = msg->date( true )->dateTime();
-        if ( msg->contentType( false )->asUnicodeString() == "text/html" ) {
+        if ( msg->contentType( false )->asUnicodeString() == QLatin1String("text/html") ) {
           textFormat = Qt::RichText;
         }
       } else {
