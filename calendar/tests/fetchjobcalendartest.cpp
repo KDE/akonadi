20e6ce511039fdff5473ff3323260d253349da08
Sergio Martins <iamsergio@gmail.com>
2011-11-19 09:19:09 +0000

    Unit-tests for FetchJobCalendar.

--- calendar/tests/fetchjobcalendartest.cpp
+++ calendar/tests/fetchjobcalendartest.cpp
@@ -18,10 +18,13 @@
 */
 
 #include "../fetchjobcalendar.h"
-
-#include <QTestEventLoop>
+#include <Akonadi/ItemFetchJob>
+#include <Akonadi/ItemCreateJob>
+#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/CollectionFetchScope>
 #include <akonadi/qtest_akonadi.h>
 
+#include <QTestEventLoop>
 
 using namespace Akonadi;
 using namespace KCalCore;
@@ -29,21 +32,78 @@ using namespace KCalCore;
 class FetchJobCalendarTest : public QObject
 {
   Q_OBJECT
+    Collection mCollection;
 
+    void createIncidence( const QString &uid )
+    {
+      Item item;
+      item.setMimeType( Event::eventMimeType() );
+      Incidence::Ptr incidence = Incidence::Ptr( new Event() );
+      incidence->setUid( uid );
+      incidence->setSummary( QLatin1String( "summary" ) );
+      item.setPayload<KCalCore::Incidence::Ptr>( incidence );
+      ItemCreateJob *job = new ItemCreateJob( item, mCollection, this );
+      AKVERIFYEXEC( job );
+    }
 
+    void fetchCollection()
+    {
+      CollectionFetchJob *job = new CollectionFetchJob( Collection::root(),
+                                                        CollectionFetchJob::Recursive,
+                                                        this );
+      // Get list of collections
+      job->fetchScope().setContentMimeTypes( QStringList() << QLatin1String( "application/x-vnd.akonadi.calendar.event" ) );
+      AKVERIFYEXEC( job );
+
+      // Find our collection
+      Collection::List collections = job->collections();
+      QVERIFY( !collections.isEmpty() );
+      mCollection = collections.first();
+
+      QVERIFY( mCollection.isValid() );
+    }
 private Q_SLOTS:
     void initTestCase()
     {
+      fetchCollection();
+      qRegisterMetaType<Akonadi::Item>("Akonadi::Item");
     }
 
-    void cleanupTestCase()
+    void testFetching()
     {
+      createIncidence( tr( "a" ) );
+      createIncidence( tr( "b" ) );
+      createIncidence( tr( "c" ) );
+      createIncidence( tr( "d" ) );
+      createIncidence( tr( "e" ) );
+      createIncidence( tr( "f" ) );
+
+      FetchJobCalendar *calendar = new FetchJobCalendar();
+      connect( calendar, SIGNAL(loadFinished(bool,QString)),
+               SLOT(handleLoadFinished(bool,QString)) );
+      QTestEventLoop::instance().enterLoop( 10 );
+      QVERIFY( !QTestEventLoop::instance().timeout() );
+
+      Incidence::List incidences = calendar->incidences();
+      QVERIFY( incidences.count() == 6 );
+      QVERIFY( calendar->item( tr( "a" ) ).isValid() );
+      QVERIFY( calendar->item( tr( "b" ) ).isValid() );
+      QVERIFY( calendar->item( tr( "c" ) ).isValid() );
+      QVERIFY( calendar->item( tr( "d" ) ).isValid() );
+      QVERIFY( calendar->item( tr( "e" ) ).isValid() );
+      QVERIFY( calendar->item( tr( "f" ) ).isValid() );
+
+      delete calendar;
     }
-
-
 
 public Q_SLOTS:
-
+  void handleLoadFinished( bool success, const QString &errorMessage )
+  {
+    if ( !success )
+      qDebug() << errorMessage;
+    QVERIFY( success );
+    QTestEventLoop::instance().exitLoop();
+  }
 };
 
 QTEST_AKONADIMAIN( FetchJobCalendarTest, GUI )
