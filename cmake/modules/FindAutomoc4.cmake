e52db90cadea6b18bbc2a8fcf248f51d06d82373
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2012-02-01 12:07:17 +0100

    Remove obsolete stuff

--- cmake/modules/FindAutomoc4.cmake
+++ cmake/modules/FindAutomoc4.cmake
@@ -39,24 +39,16 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 
-# check if we are inside KDESupport
-if("${KDESupport_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-   # when building this project as part of kdesupport
-   set(AUTOMOC4_CONFIG_FILE "${KDESupport_SOURCE_DIR}/automoc/Automoc4Config.cmake")
-else("${KDESupport_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-   # when building this project outside kdesupport
+# CMAKE_[SYSTEM_]PREFIX_PATH exists starting with cmake 2.6.0
+file(TO_CMAKE_PATH "$ENV{CMAKE_PREFIX_PATH}" _env_CMAKE_PREFIX_PATH)
+file(TO_CMAKE_PATH "$ENV{CMAKE_LIBRARY_PATH}" _env_CMAKE_LIBRARY_PATH)
 
-   # CMAKE_[SYSTEM_]PREFIX_PATH exists starting with cmake 2.6.0
-   file(TO_CMAKE_PATH "$ENV{CMAKE_PREFIX_PATH}" _env_CMAKE_PREFIX_PATH)
-   file(TO_CMAKE_PATH "$ENV{CMAKE_LIBRARY_PATH}" _env_CMAKE_LIBRARY_PATH)
-
-   find_file(AUTOMOC4_CONFIG_FILE NAMES Automoc4Config.cmake 
-             PATH_SUFFIXES automoc4 lib/automoc4 lib64/automoc4
-             PATHS ${_env_CMAKE_PREFIX_PATH} ${CMAKE_PREFIX_PATH} ${CMAKE_SYSTEM_PREFIX_PATH} 
-                   ${_env_CMAKE_LIBRARY_PATH} ${CMAKE_LIBRARY_PATH} ${CMAKE_SYSTEM_LIBRARY_PATH}
-                   ${CMAKE_INSTALL_PREFIX}
-             NO_DEFAULT_PATH )
-endif("${KDESupport_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
+find_file(AUTOMOC4_CONFIG_FILE NAMES Automoc4Config.cmake
+          PATH_SUFFIXES automoc4 lib/automoc4 lib64/automoc4
+          PATHS ${_env_CMAKE_PREFIX_PATH} ${CMAKE_PREFIX_PATH} ${CMAKE_SYSTEM_PREFIX_PATH}
+                ${_env_CMAKE_LIBRARY_PATH} ${CMAKE_LIBRARY_PATH} ${CMAKE_SYSTEM_LIBRARY_PATH}
+                ${CMAKE_INSTALL_PREFIX}
+          NO_DEFAULT_PATH )
 
 
 if(AUTOMOC4_CONFIG_FILE)
@@ -72,10 +64,10 @@ if (AUTOMOC4_FOUND)
    endif (NOT Automoc4_FIND_QUIETLY)
 else (AUTOMOC4_FOUND)
    if (Automoc4_FIND_REQUIRED)
-      message(FATAL_ERROR "Did not find automoc4 (part of kdesupport).")
+      message(FATAL_ERROR "Did not find automoc4.")
    else (Automoc4_FIND_REQUIRED)
       if (NOT Automoc4_FIND_QUIETLY)
-         message(STATUS "Did not find automoc4 (part of kdesupport).")
+         message(STATUS "Did not find automoc4.")
       endif (NOT Automoc4_FIND_QUIETLY)
    endif (Automoc4_FIND_REQUIRED)
 endif (AUTOMOC4_FOUND)
