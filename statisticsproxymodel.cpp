76d286504994176ae2240d66bce6e32824d9d6e0
Montel Laurent <montel@kde.org>
2012-02-08 09:39:35 +0100

    Backport for Bug 233414 - Folder size of collapsed folders does not
    include subfolder size
    (this class was forked in libkdepim)

--- statisticsproxymodel.cpp
+++ statisticsproxymodel.cpp
@@ -53,6 +53,25 @@ class StatisticsProxyModel::Private
       return mParent->sourceModel()->columnCount( mParent->mapToSource( parent ) );
     }
 
+    void getCountRecursive( const QModelIndex &index, qint64 &totalSize ) const
+    {
+      Collection collection = qvariant_cast<Collection>( index.data( EntityTreeModel::CollectionRole ) );
+      // Do not assert on invalid collections, since a collection may be deleted
+      // in the meantime and deleted collections are invalid.
+      if ( collection.isValid() ) {
+        CollectionStatistics statistics = collection.statistics();
+        totalSize += qMax( 0LL, statistics.size() );
+        if ( index.model()->hasChildren( index ) ) {
+          const int rowCount = index.model()->rowCount( index );
+          for ( int row = 0; row < rowCount; row++ ) {
+            static const int column = 0;
+            getCountRecursive( index.model()->index( row, column, index ),  totalSize );
+          }
+        }
+      }
+    }
+
+
     QString toolTipForCollection( const QModelIndex &index, const Collection &collection )
     {
       QString bckColor = QApplication::palette().color( QPalette::ToolTipBase ).name();
@@ -99,9 +118,20 @@ class StatisticsProxyModel::Private
         }
       }
 
+      qint64 currentFolderSize( collection.statistics().size() );
       tipInfo += QString::fromLatin1(
         "      <strong>%1</strong>: %2<br>\n"
-        ).arg( i18n( "Storage Size" ) ).arg( KIO::convertSize( (KIO::filesize_t)( collection.statistics().size() ) ) );
+        ).arg( i18n( "Storage Size" ) ).arg( KIO::convertSize( (KIO::filesize_t)( currentFolderSize ) ) );
+
+
+      qint64 totalSize = 0;
+      getCountRecursive( index, totalSize );
+      totalSize -= currentFolderSize;
+      if (totalSize > 0 ) {
+        tipInfo += QString::fromLatin1(
+          "<strong>%1</strong>: %2<br>"
+          ).arg( i18n("Subfolder Storage Size") ).arg( KIO::convertSize( (KIO::filesize_t)( totalSize ) ) );
+      }
 
 
      QString iconName = CollectionUtils::defaultIconName( collection );
