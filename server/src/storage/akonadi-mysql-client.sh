d4659ad67047c4d9d7a77345249d8ac5898ae57c
Volker Krause <vkrause@kde.org>
2012-07-15 14:36:13 +0200

    Make this helper script multi-instance aware.

--- server/src/storage/akonadi-mysql-client.sh
+++ server/src/storage/akonadi-mysql-client.sh
@@ -2,5 +2,14 @@
 # connect to mysqld started by akonadi
 # useful for developing
 
-akonadisocket="$HOME/.local/share/akonadi/socket-`hostname`/mysql.socket"
+if [ -z "$1" ]; then
+  akonadisocket="$HOME/.local/share/akonadi/socket-`hostname`/mysql.socket"
+else
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [instance identifier]"
+    exit 1;
+  fi
+  akonadisocket="$HOME/.local/share/akonadi/instance/$1/socket-`hostname`/mysql.socket"
+fi
+
 mysql --socket=$akonadisocket akonadi
