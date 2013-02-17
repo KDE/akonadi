7cadfbff3142b751722073823135ebfa67bf2c9d
Allen Winter <winter@kde.org>
2013-02-17 12:39:12 -0500

    fix Laurent's email address in the copyright

--- contact/actions/qekigadialer.cpp
+++ contact/actions/qekigadialer.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2013 Montel Laurent <montel.org>
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
