74ae8e5a31a0cd3dce244697617142aa3d2935a2
Sergio Martins <iamsergio@gmail.com>
2013-06-17 21:20:03 +0100

    Remove @since.
    Not needed.

--- calendar/etmcalendar.h
+++ calendar/etmcalendar.h
@@ -73,8 +73,6 @@ public:
    * Constructs a new ETMCalendar that will only load the specified mime types.
    * Use this ctor to ignore journals or to-dos for example.
    * If no mime types are specified, all mime types will be used.
-   *
-   * @since 4.12
    */
   explicit ETMCalendar( const QStringList &mimeTypes, QObject *parent = 0 );
 
