54cffdc4a13cebc596a5c08bb847c287cc3a17c4
Montel Laurent <montel@kde.org>
2013-08-20 18:27:52 +0200

    It's a good idea to give year info

--- gid/gidextractor_p.h
+++ gid/gidextractor_p.h
@@ -1,5 +1,5 @@
 /*
-    Author: Christian Mollekopf <mollekopf@kolabsys.com>
+    Author: (2013) Christian Mollekopf <mollekopf@kolabsys.com>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
