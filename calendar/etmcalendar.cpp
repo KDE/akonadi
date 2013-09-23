e7404b145e2104034225d8fa68937163a7432268
Sergio Martins <iamsergio@gmail.com>
2013-09-23 21:37:45 +0100

    Also update internal Item not only the payload.
    The Item itself is different, it has a different revision. Leaving
    old items laying around result in conflicts in future ItemModifyJobs.

--- calendar/etmcalendar.cpp
+++ calendar/etmcalendar.cpp
@@ -409,6 +409,7 @@ void ETMCalendarPrivate::updateItem( const Akonadi::Item &item )
   IncidenceBase::Ptr existingIncidence = q->incidence( newIncidence->uid(), newIncidence->recurrenceId() );
   if ( existingIncidence ) {
     *(existingIncidence.data()) = *( newIncidence.data() );
+    mItemById.insert( item.id(), item ); // The item needs updating too, revision changed.
     mItemsByCollection.insert( item.storageCollectionId(), item );
   } else if ( mItemById.contains( item.id() ) ) {
     // The item changed it's UID, update our maps, the Google resource changes the UID when we create incidences.
