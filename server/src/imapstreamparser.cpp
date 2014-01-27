b1d6abbae19acf13c6711396faad9304a657073a
Christian Mollekopf <chrigi_1@fastmail.fm>
2014-01-27 12:09:32 +0100

    Another out of bounds fix from kimap.

--- server/src/imapstreamparser.cpp
+++ server/src/imapstreamparser.cpp
@@ -380,7 +380,8 @@ QList<QByteArray> ImapStreamParser::readParenthesizedList()
 
       // We might sometime get some unwanted CRLF, but we're still not at the end
       // of the list, would make further string reads fail so eat the CRLFs.
-      while ( m_data[m_position]=='\r' || m_data[m_position]=='\n' ) {
+      while ( ( m_position < m_data.size() ) &&
+        ( m_data[m_position]=='\r' || m_data[m_position]=='\n' ) ) {
         m_position++;
       }
 
