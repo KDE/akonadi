d53c80c7e0d09dc9e243fa1c1cf1683b74075d94
Dan Vrátil <dvratil@redhat.com>
2014-03-20 11:44:12 +0100

    Fix QObject warning on Akonadi start

--- server/src/search/searchmanager.cpp
+++ server/src/search/searchmanager.cpp
@@ -66,12 +66,9 @@ SearchManagerThread::~SearchManagerThread()
 
 void SearchManagerThread::run()
 {
-  DataStore::self();
-  SearchManager manager( mSearchEngines, this );
-
+  SearchManager *manager = new SearchManager( mSearchEngines );
   exec();
-
-  DataStore::self()->close();
+  delete manager;
 }
 
 SearchManager::SearchManager( const QStringList &searchEngines, QObject *parent )
@@ -113,11 +110,14 @@ SearchManager::SearchManager( const QStringList &searchEngines, QObject *parent 
   mSearchUpdateTimer->setSingleShot( true );
   connect( mSearchUpdateTimer, SIGNAL(timeout()),
            this, SLOT(searchUpdateTimeout()) );
+
+  DataStore::self();
 }
 
 SearchManager::~SearchManager()
 {
   qDeleteAll( mEngines );
+  DataStore::self()->close();
   sInstance = 0;
 }
 
