7baaafdcb15fdce2dbb0e2a6e9cb05fe635445ea
Montel Laurent <montel@kde.org>
2014-04-08 23:28:19 +0200

    Move to own directory

--- calendar/Messages.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-$EXTRACTRC *.ui *.kcfg >> rc.cpp || exit 11
-$XGETTEXT *.cpp *.h -o $podir/libakonadi-calendar.pot
-rm -f rc.cpp
-
--- contact/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC *.ui actions/*.kcfg.cmake *.kcfg >> rc.cpp || exit 11
-$XGETTEXT *.cpp *.h actions/*.cpp editor/*.cpp editor/im/*.cpp -o $podir/akonadicontact.pot
-rm -f rc.cpp
