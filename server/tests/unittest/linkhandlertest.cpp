6c472032f494d3bb55e05a6b93b8210b6ab5405d
Dan Vrátil <dvratil@redhat.com>
2014-05-12 13:36:04 +0200

    Extend LinkHandlerTest by tests for UNLINK command

--- server/tests/unittest/linkhandlertest.cpp
+++ server/tests/unittest/linkhandlertest.cpp
@@ -60,7 +60,7 @@ public:
 
 
 private Q_SLOTS:
-    void testLinkHandler_data()
+    void testLink_data()
     {
         QTest::addColumn<Scope>("scope");
         QTest::addColumn<QByteArray>("command");
@@ -121,7 +121,7 @@ private Q_SLOTS:
         QTest::newRow("HRID collection, RID items") << scope << command << notification << context << false;
     }
 
-    void testLinkHandler()
+    void testLink()
     {
         QFETCH(Scope, scope);
         QFETCH(QByteArray, command);
@@ -171,6 +171,120 @@ private Q_SLOTS:
             }
         }
     }
+
+    void testUnlink_data()
+    {
+        QTest::addColumn<Scope>("scope");
+        QTest::addColumn<QByteArray>("command");
+        QTest::addColumn<Akonadi::NotificationMessageV3>("notification");
+        QTest::addColumn<CommandContext>("context");
+        QTest::addColumn<bool>("expectFail");
+
+        Scope scope(Scope::Uid);
+        QByteArray command;
+        CommandContext context;
+        NotificationMessageV3 notification;
+
+        command = "1 UID UNLINK 3 1:3\n";
+        QTest::newRow("non-virtual collection") << scope << command << NotificationMessageV3() << context << true;
+
+        notification.setType(NotificationMessageV2::Items);
+        notification.setOperation(NotificationMessageV2::Unlink);
+        notification.addEntity(1, QLatin1String("A"), QString(), QLatin1String("application/octet-stream"));
+        notification.addEntity(2, QLatin1String("B"), QString(), QLatin1String("application/octet-stream"));
+        notification.addEntity(3, QLatin1String("C"), QString(), QLatin1String("application/octet-stream"));
+        notification.setParentCollection(6);
+        notification.setResource("akonadi_fake_resource_with_virtual_collections_0");
+        command = "1 UID UNLINK 6 1:3\n";
+        QTest::newRow("normal") << scope << command << notification << context << false;
+
+        notification.clearEntities();
+        notification.addEntity(4, QLatin1String("D"), QString(), QLatin1String("application/octet-stream"));
+        command = "1 UID UNLINK 6 4 2048\n";
+        QTest::newRow("existent and non-existent item") << scope << command << notification << context << false;
+
+        command = "1 UID UNLINK 6 4096\n";
+        QTest::newRow("non-existent item only") << scope << command << NotificationMessageV3() << context << false;
+
+        context.setCollection(Collection::retrieveByName(QLatin1String("Collection B")));
+        QVERIFY(context.collection().isValid());
+        command = "1 UID UNLINK 6 RID (\"F\" \"G\")\n";
+        notification.clearEntities();
+        notification.clearEntities();
+        notification.addEntity(6, QLatin1String("F"), QString(), QLatin1String("application/octet-stream"));
+        notification.addEntity(7, QLatin1String("G"), QString(), QLatin1String("application/octet-stream"));
+        QTest::newRow("RID items") << scope << command << notification << context << false;
+
+        scope = Scope(Scope::HierarchicalRid);
+        context.setCollection(Collection());
+        context.setResource(Resource::retrieveByName(QLatin1String("akonadi_fake_resource_with_virtual_collections_0")));
+        QVERIFY(context.resource().isValid());
+        command = "1 HRID UNLINK ((-1, \"virtual2\") (-1, \"virtual\") (-1, \"\")) UID 5\n";
+        notification.setParentCollection(7);
+        notification.clearEntities();
+        notification.addEntity(5, QLatin1String("E"), QString(), QLatin1String("application/octet-stream"));
+        QTest::newRow("HRID collection") << scope << command << notification << context << false;
+
+        command= "1 HRID UNLINK ((-1, \"virtual2\") (-1, \"virtual\") (-1, \"\")) RID \"H\"\n";
+        context.setCollection(Collection::retrieveByName(QLatin1String("Collection B")));
+        QVERIFY(context.collection().isValid());
+        notification.clearEntities();
+        notification.addEntity(8, QLatin1String("H"), QString(), QLatin1String("application/octet-stream"));
+        QTest::newRow("HRID collection, RID items") << scope << command << notification << context << false;
+    }
+
+
+    void testUnlink()
+    {
+        QFETCH(Scope, scope);
+        QFETCH(QByteArray, command);
+        QFETCH(NotificationMessageV3, notification);
+        QFETCH(CommandContext, context);
+        QFETCH(bool, expectFail);
+
+        // Will be deleted by Connection after test is finished
+        Link *handler = new Link(scope.scope(), false);
+
+        FakeConnection *fakeConnection = mServer.connection(handler, command, context);
+
+        NotificationCollector *collector = fakeConnection->storageBackend()->notificationCollector();
+        QSignalSpy responseSpy(fakeConnection, SIGNAL(responseAvailable(Akonadi::Server::Response)));
+        QSignalSpy notificationSpy(collector, SIGNAL(notify(Akonadi::NotificationMessageV3::List)));
+
+        // Run the actual test
+        fakeConnection->run();
+
+        QCOMPARE(responseSpy.count(), 1);
+        const Response response = responseSpy.takeFirst().first().value<Response>();
+        if (expectFail) {
+            QVERIFY(response.resultCode() != Response::OK);
+            QCOMPARE(notificationSpy.count(), 0);
+        } else {
+            if (!response.resultCode() == Response::OK) {
+                qDebug() << "Result code: " << response.resultCode();
+                qDebug() << "Response: " << response.asString();
+                QFAIL("Incorrect result code, expected OK");
+            }
+            if (notification.isValid()) {
+                QCOMPARE(notificationSpy.count(), 1);
+                const NotificationMessageV3::List notifications = notificationSpy.takeFirst().first().value<NotificationMessageV3::List>();
+                QCOMPARE(notifications.count(), 1);
+                QCOMPARE(notifications.first(), notification);
+            } else {
+                QVERIFY(notificationSpy.isEmpty() || notificationSpy.takeFirst().first().value<NotificationMessageV3::List>().isEmpty());
+            }
+        }
+
+
+        Q_FOREACH (const NotificationMessageV2::Entity &entity, notification.entities()) {
+            if (expectFail) {
+              QVERIFY(Collection::relatesToPimItem(notification.parentCollection(), entity.id));
+            } else {
+              QVERIFY(!Collection::relatesToPimItem(notification.parentCollection(), entity.id));
+            }
+        }
+    }
+
 };
 
 AKTEST_FAKESERVER_MAIN(LinkHandlerTest)
