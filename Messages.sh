3676d973e9b76b157c199a452c2869d8b562492f
Montel Laurent <montel@kde.org>
2014-06-01 11:11:28 +0200

    Change message filename to allow to co-install kde4/kf5 if we want

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . src -name "*.ui"` >> rc.cpp
-$XGETTEXT `find . src -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi.pot
+$XGETTEXT `find . src -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi5.pot
