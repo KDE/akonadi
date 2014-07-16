eb83c5837cc3b0fe961511ffbe8e4a66d105f4ac
Montel Laurent <montel@kde.org>
2014-07-16 22:39:43 +0200

    Make it compile with -DQT_NO_CAST_FROM_ASCII -DQT_NO_CAST_TO_ASCII

--- src/selftest/main.cpp
+++ src/selftest/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 
   QApplication app(argc, argv);
   QCoreApplication::setApplicationName(QStringLiteral("akonadiselftest"));
-  QCoreApplication::setApplicationVersion("1.0");
+  QCoreApplication::setApplicationVersion(QLatin1String("1.0"));
   parser.process(app);
 
   SelfTestDialog dlg;
