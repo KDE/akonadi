2d2a10994bf53dac05169338ac9c5f6f852a35ab
Kevin Ottens <ervin@kde.org>
2014-08-05 16:07:15 +0200

    Make it build
    Conflicts:
    	akonadi/resourcebase.cpp

--- resourcebase.cpp
+++ resourcebase.cpp
@@ -726,6 +726,9 @@ void ResourceBasePrivate::changeCommittedResult(KJob *job)
         }
         mChangeRecorder->d_ptr->invalidateCache(static_cast<CollectionModifyJob *>(job)->collection());
     } else {
+        if (job->error()) {
+            emit q->error(i18nc("@info", "Updating local items failed: %1.", job->errorText()));
+        }
         // Item and tag cache is invalidated by modify job
     }
 
