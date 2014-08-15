c724f72af9e3f1611d4fc88666764bc72689982f
Albert Astals Cid <aacid@kde.org>
2014-08-15 23:51:35 +0200

    Fix Messages.sh
    CCMAIL: montel@kde.org

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$EXTRACTRC `find . src -name "*.ui"` >> rc.cpp
-$XGETTEXT `find . src -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi5.pot
+$EXTRACTRC `find -name "*.ui"` >> rc.cpp
+$XGETTEXT `find -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi5.pot
