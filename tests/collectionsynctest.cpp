a82745a459156317f5fb394a21c5caf0b90ffdfd
Dan Vrátil <dvratil@redhat.com>
2014-09-25 19:52:35 +0200

    CollectionSync: Optimized algorithm
    This is combination of commits by Daniel Vratil.
    
    * The weakest point of CollectionSync is in findMatchingLocalNode() method
    that needs to traverse the LocalNode tree every time from up down to find the
    LocalNode* that matches the given remote Collection, which is very inefficient,
    especially in the case of initial synchronization.
    
    * Calling parentCollection() is expensive (even though Collection is implicitly shared),
    because of Entity::Entity(const Entity &other) which is very ineffective (but probably
    not possible to fix without API changes), so we try to reduce the number of calls to
    minimum by calling it once and then storing the parent collection in a variable.
    
    * The new algorithm can sync 50 000 collections in about 2 minutes, which is 4 times
    faster then what I ever was able to optimize the original algorithm.
    
    The algorithm stores the \"tree\" structure of collections differently than the old
    one: it stores list of collections for each parent in a QHash, and then starts
    recursively querying Akonadi for collections, easilly comparing list of descendats
    it got from Akonadi with the list in the QHash for the same parent RID chain.
    
    * With non-hieararchical RIDs, we don't have the full parent chain available -
    each collection only refers to RID of its parent, but we know nothing of the
    parent's parent. To correctly match those, we need to compare only parts of
    RemoteID chains.
    
    * Instead of listing children for each Collection individually, we request all Collections
    at once and then process them in one go. This means that the algorithm will require more
    memory, but it will be much faster when dealing with large amounts of Collections.
    
    * This adds two banchmarks for CollectionSync: one that does an initial sync
    of 60 000 Collections, and one that does incremental sync against 60 000
    Collections.
    
    On my computer, the results are around 160 seconds for benchmarkInitialSync()
    and around 8 seconds for benchmarkIncrementalSync().
    
    The benchmarks are disabled by default, because the cleanup takes insane
    amount of time (I blame poor implementation of DELETE command on the server).

--- tests/collectionsynctest.cpp
+++ tests/collectionsynctest.cpp
@@ -33,8 +33,10 @@
 
 #include <QtCore/QObject>
 #include <QSignalSpy>
+#include <QEventLoop>
 
 #include <qtest_akonadi.h>
+#include <resourceselectjob_p.h>
 
 using namespace Akonadi;
 
@@ -66,6 +68,85 @@ class CollectionSyncTest : public QObject
       QTest::newRow( "akonadi_knut_resource_0 hierarchical RID" ) << true << "akonadi_knut_resource_0";
       QTest::newRow( "akonadi_knut_resource_1 hierarchical RID" ) << true << "akonadi_knut_resource_1";
       QTest::newRow( "akonadi_knut_resource_2 hierarchical RID" ) << true << "akonadi_knut_resource_2";
+    }
+
+    Collection createCollection(const QString &name, const QString &remoteId, const Collection &parent)
+    {
+        Collection c;
+        c.setName(name);
+        c.setRemoteId(remoteId);
+        c.setParentCollection(parent);
+        c.setResource("akonadi_knut_resource_0");
+        c.setContentMimeTypes(QStringList() << Collection::mimeType());
+        return c;
+    }
+
+    Collection::List prepareBenchmark()
+    {
+        Collection::List collections = fetchCollections(QLatin1String("akonadi_knut_resource_0"));
+
+        ResourceSelectJob *resJob = new ResourceSelectJob("akonadi_knut_resource_0");
+        Q_ASSERT(resJob->exec());
+
+        Collection root;
+        Q_FOREACH (const Collection &col, collections) {
+            if (col.parentCollection() == Collection::root()) {
+                root = col;
+                break;
+            }
+        }
+        Q_ASSERT(root.isValid());
+
+        // we must build on top of existing collections, because only resource is
+        // allowed to create top-level collection
+        Collection::List baseCollections;
+        for (int i = 0; i < 20; ++i) {
+            baseCollections << createCollection(QString::fromLatin1("Base Col %1").arg(i), QString::fromLatin1("/baseCol%1").arg(i), root);
+        }
+        collections += baseCollections;
+
+        const Collection shared = createCollection(QLatin1String("Shared collections"), QLatin1String("/shared"), root);
+        baseCollections << shared;
+        collections << shared;
+        for (int i = 0; i < 10000; ++i) {
+            const Collection col = createCollection(QString::fromLatin1("Shared Col %1").arg(i), QString::fromLatin1("/shared%1").arg(i), shared);
+              collections << col;
+              for (int j = 0; j < 6; ++j) {
+                  collections << createCollection(QString::fromLatin1("Shared Subcol %1-%2").arg(i).arg(j), QString::fromLatin1("/shared%1-%2").arg(i).arg(j), col);
+              }
+        }
+        return collections;
+    }
+
+    CollectionSync *prepareBenchmarkSyncer(const Collection::List &collections)
+    {
+        CollectionSync *syncer = new CollectionSync("akonadi_knut_resource_0");
+        connect(syncer, SIGNAL(percent(KJob*,ulong)), this, SLOT(syncBenchmarkProgress(KJob*,ulong)));
+        syncer->setHierarchicalRemoteIds(false);
+        syncer->setRemoteCollections(collections);
+        return syncer;
+    }
+
+    void cleanupBenchmark(const Collection::List &collections)
+    {
+        Collection::List baseCols;
+        Q_FOREACH(const Collection &col, collections) {
+            if (col.remoteId().startsWith(QLatin1String("/baseCol")) || col.remoteId() == QLatin1String("/shared")) {
+                baseCols << col;
+            }
+        }
+        Q_FOREACH (const Collection &col, baseCols) {
+            CollectionDeleteJob *del = new CollectionDeleteJob(col);
+            AKVERIFYEXEC(del);
+        }
+    }
+
+
+  public Q_SLOTS:
+    void syncBenchmarkProgress(KJob *job, ulong percent)
+    {
+        Q_UNUSED(job);
+        qDebug() << "CollectionSync progress:" <<  percent << "%";
     }
 
   private Q_SLOTS:
@@ -299,6 +380,40 @@ class CollectionSyncTest : public QObject
         }
       }
     }
+
+// Disabled by default, because they take ~15 minutes to complete
+#if 0
+    void benchmarkInitialSync()
+    {
+        const Collection::List collections = prepareBenchmark();
+
+        CollectionSync *syncer = prepareBenchmarkSyncer(collections);
+
+        QBENCHMARK_ONCE {
+            AKVERIFYEXEC(syncer);
+        }
+
+        cleanupBenchmark(collections);
+    }
+
+    void benchmarkIncrementalSync()
+    {
+        const Collection::List collections = prepareBenchmark();
+
+        // First populate Akonadi with Collections
+        CollectionSync *syncer = prepareBenchmarkSyncer(collections);
+        AKVERIFYEXEC(syncer);
+
+        // Now create a new syncer to benchmark the incremental sync
+        syncer = prepareBenchmarkSyncer(collections);
+
+        QBENCHMARK_ONCE {
+            AKVERIFYEXEC(syncer);
+        }
+
+        cleanupBenchmark(collections);
+    }
+#endif
 };
 
 QTEST_AKONADIMAIN( CollectionSyncTest, NoGUI )
