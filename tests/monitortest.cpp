dd9753e6a1e0819c1772dd59ce41c95bb341f43f
Christian Mollekopf <chrigi_1@fastmail.fm>
2014-10-16 20:43:19 +0200

    MonitorTest: cacheOnly
    Since we set the resource offline we need to set cache only.
    
    Or should we not hit the resource anyways if all data is already in akonadi?

--- tests/monitortest.cpp
+++ tests/monitortest.cpp
@@ -78,6 +78,7 @@ void MonitorTest::testMonitor()
   monitor->setCollectionMonitored( Collection::root() );
   monitor->fetchCollection( fetchCol );
   monitor->itemFetchScope().fetchFullPayload();
+  monitor->itemFetchScope().setCacheOnly(true);
 
   // monitor signals
   qRegisterMetaType<Akonadi::Collection>();
