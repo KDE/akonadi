KDE/akonadi

commit 48373d50e9c875828827389db92f9f442425405d

Make sure CollectionScheduler actually lives in a QThread
The previous implementation caused CollectionScheduler slots to be executed
in the mainthread, effectively blocking things like notifications subscribtions
when the scheduler would get stuck for a longer period of time (like CacheCleaner
waiting for transaction lock)

src/server/akonadi.cpp
@@ -72,8 +72,8 @@ AkonadiServer *AkonadiServer::s_instance = 0;
 
 AkonadiServer::AkonadiServer(QObject *parent)
     : QLocalServer(parent)
-    , mCacheCleaner(0)
-    , mIntervalChecker(0)
+    , mCacheCleanerThread(0)
+    , mIntervalCheckerThread(0)
     , mStorageJanitor(0)
     , mItemRetrievalThread(0)
     , mAgentSearchManagerThread(0)
@@ -188,12 +188,12 @@ bool AkonadiServer::init()
     }
 
     if (settings.value(QStringLiteral("Cache/EnableCleaner"), true).toBool()) {
-        mCacheCleaner = new CacheCleaner(this);
-        mCacheCleaner->start(QThread::IdlePriority);
+        mCacheCleanerThread = new CollectionSchedulerThread<CacheCleaner>(this);
+        mCacheCleanerThread->start(QThread::IdlePriority);
     }
 
-    mIntervalChecker = new IntervalCheck(this);
-    mIntervalChecker->start(QThread::IdlePriority);
+    mIntervalCheckerThread = new CollectionSchedulerThread<IntervalCheck>(this);
+    mIntervalCheckerThread->start(QThread::IdlePriority);
 
     mStorageJanitor = new StorageJanitorThread;
     mStorageJanitor->start(QThread::IdlePriority);
@@ -267,8 +267,8 @@ bool AkonadiServer::quit()
     mAlreadyShutdown = true;
 
     akDebug() << "terminating service threads";
-    quitThread(mCacheCleaner);
-    quitThread(mIntervalChecker);
+    quitThread(mCacheCleanerThread);
+    quitThread(mIntervalCheckerThread);
     quitThread(mStorageJanitor);
     quitThread(mItemRetrievalThread);
     mAgentSearchManagerThread->stop();
@@ -403,10 +403,10 @@ void AkonadiServer::serviceOwnerChanged(const QString &name, const QString &oldO
 
 CacheCleaner *AkonadiServer::cacheCleaner()
 {
-    return mCacheCleaner;
+    return mCacheCleanerThread->scheduler();
 }
 
 IntervalCheck *AkonadiServer::intervalChecker()
 {
-    return mIntervalChecker;
+    return mIntervalCheckerThread->scheduler();
 }

src/server/akonadi.h
@@ -31,12 +31,14 @@ namespace Akonadi {
 namespace Server {
 
 class ConnectionThread;
-class CacheCleaner;
 class SearchManagerThread;
 class ItemRetrievalThread;
 class SearchTaskManagerThread;
 class StorageJanitorThread;
+class CacheCleaner;
 class IntervalCheck;
+template<typename T>
+class CollectionSchedulerThread;
 
 class AkonadiServer : public QLocalServer
 {
@@ -80,8 +82,8 @@ private:
 protected:
     AkonadiServer(QObject *parent = 0);
 
-    CacheCleaner *mCacheCleaner;
-    IntervalCheck *mIntervalChecker;
+    CollectionSchedulerThread<CacheCleaner> *mCacheCleanerThread;
+    CollectionSchedulerThread<IntervalCheck> *mIntervalCheckerThread;
     StorageJanitorThread *mStorageJanitor;
     ItemRetrievalThread *mItemRetrievalThread;
     SearchTaskManagerThread *mAgentSearchManagerThread;

src/server/cachecleaner.h
@@ -55,8 +55,8 @@ private:
 };
 
 /**
-  Cache cleaner thread.
-*/
+  Cache cleaner.
+ */
 class CacheCleaner : public CollectionScheduler
 {
     Q_OBJECT

src/server/collectionscheduler.cpp
@@ -109,30 +109,18 @@ private:
 using namespace Akonadi::Server;
 
 CollectionScheduler::CollectionScheduler(QObject *parent)
-    : QThread(parent)
+    : QObject(parent)
     , mMinInterval(5)
 {
     // make sure we are created from the main thread, ie. before all other threads start to potentially use us
     Q_ASSERT(QThread::currentThread() == QCoreApplication::instance()->thread());
 
-    mScheduler = new PauseableTimer(this);
-    mScheduler->setSingleShot(true);
-    connect(mScheduler, SIGNAL(timeout()),
-            this, SLOT(schedulerTimeout()));
+    QMetaObject::invokeMethod(this, "initScheduler", Qt::QueuedConnection);
 }
 
 CollectionScheduler::~CollectionScheduler()
 {
-}
-
-void CollectionScheduler::run()
-{
-    DataStore::self();
-
-    QTimer::singleShot(0, this, SLOT(initScheduler()));
-    exec();
-
-    DataStore::self()->close();
+    delete mScheduler;
 }
 
 void CollectionScheduler::inhibit(bool inhibit)
@@ -140,9 +128,11 @@ void CollectionScheduler::inhibit(bool inhibit)
     if (inhibit && mScheduler->isActive() && !mScheduler->isPaused()) {
         const bool success = QMetaObject::invokeMethod(mScheduler, "pause", Qt::QueuedConnection);
         Q_ASSERT(success);
+        Q_UNUSED(success);
     } else if (!inhibit && mScheduler->isPaused()) {
         const bool success = QMetaObject::invokeMethod(mScheduler, "resume", Qt::QueuedConnection);
         Q_ASSERT(success);
+        Q_UNUSED(success);
     }
 }
 
@@ -276,6 +266,11 @@ void CollectionScheduler::scheduleCollection(Collection collection, bool shouldS
 
 void CollectionScheduler::initScheduler()
 {
+    mScheduler = new PauseableTimer();
+    mScheduler->setSingleShot(true);
+    connect(mScheduler, SIGNAL(timeout()),
+            this, SLOT(schedulerTimeout()));
+
     // Only retrieve enabled collections and referenced collections, we don't care
     // about anything else
     SelectQueryBuilder<Collection> qb;

src/server/collectionscheduler.h
@@ -33,7 +33,33 @@ namespace Server {
 class Collection;
 class PauseableTimer;
 
-class CollectionScheduler : public QThread
+template<typename T>
+class CollectionSchedulerThread : public QThread
+{
+public:
+    explicit CollectionSchedulerThread(QObject *parent = Q_NULLPTR)
+        : QThread(parent)
+    {
+        mScheduler = new T();
+        mScheduler->moveToThread(this);
+    }
+
+    ~CollectionSchedulerThread()
+    {
+        delete mScheduler;
+    }
+
+    T *scheduler() const
+    {
+        return mScheduler;
+    }
+
+private:
+    T *mScheduler;
+};
+
+
+class CollectionScheduler : public QObject
 {
     Q_OBJECT
 
@@ -56,8 +82,6 @@ public:
     int minimumInterval() const;
 
 protected:
-    virtual void run();
-
     virtual bool shouldScheduleCollection(const Collection &collection) = 0;
     virtual bool hasChanged(const Collection &collection, const Collection &changed) = 0;
     /**

src/server/storagejanitor.h
@@ -35,7 +35,7 @@ class StorageJanitorThread : public QThread
 public:
     explicit StorageJanitorThread(QObject *parent = 0);
 protected:
-    virtual void run();
+    void run();
 };
 
 /**

commit 7c496429a70f77e81931056971d086ce5c815c30

Fix crash when running unit-tests with FakeAkonadiServer
Fixes crash introduced in 48373d5. Makes CI and Volker happy.

src/server/akonadi.cpp
@@ -403,10 +403,18 @@ void AkonadiServer::serviceOwnerChanged(const QString &name, const QString &oldO
 
 CacheCleaner *AkonadiServer::cacheCleaner()
 {
-    return mCacheCleanerThread->scheduler();
+    if (mCacheCleanerThread) {
+        return mCacheCleanerThread->scheduler();
+    }
+
+    return Q_NULLPTR;
 }
 
 IntervalCheck *AkonadiServer::intervalChecker()
 {
-    return mIntervalCheckerThread->scheduler();
+    if (mIntervalCheckerThread) {
+        return mIntervalCheckerThread->scheduler();
+    }
+
+    return Q_NULLPTR;
 }