48e5d2756ee94c474212df4de785393e9d5e46b2
Daniel Vrátil <dvratil@kde.org>
2016-01-18 23:28:12 +0100

    Revert "Remove akonadi-prefix.h"
    This reverts commit c85df6aa091b39db682e26d27d99fc38714d0d02.
    This reverts commit 2cb9292ed648a49cdef7a22e3679f7acfc28e148.

---
+++ akonadi-prefix.h.cmake
@@ -0,0 +1,7 @@
+/* This file contains all the paths that change when changing the installation prefix */
+
+#define AKONADIPREFIX "${CMAKE_INSTALL_PREFIX}"
+#define AKONADIDATA   "${SHARE_INSTALL_PREFIX}"
+#define AKONADICONFIG "${CONFIG_INSTALL_DIR}"
+#define AKONADILIB    "${LIB_INSTALL_DIR}"
+#define AKONADIBUNDLEPATH "${AKONADI_BUNDLE_PATH}"
