b8e964ac95b7409347337ad05d89b38ea29f827e
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-05-29 12:44:11 +0200

    trivial: Fix typo in copyright year

--- autotests/libs/itemdeletetest.cpp
+++ autotests/libs/itemdeletetest.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 20089 Volker Krause <vkrause@kde.org>
+    Copyright (c) 2009 Volker Krause <vkrause@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
