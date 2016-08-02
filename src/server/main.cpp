efc8f6a384c0b0568ef26cc45ae3283df7686867
Montel Laurent <montel@kde.org>
2016-08-02 13:15:00 +0200

    QT_STATICPLUGIN is not used

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -33,12 +33,6 @@
 
 #include <cstdlib>
 
-#ifdef QT_STATICPLUGIN
-#include <QtPlugin>
-
-Q_IMPORT_PLUGIN(qsqlite3)
-#endif
-
 void shutdownHandler(int)
 {
     qCDebug(AKONADISERVER_LOG) << "Shutting down AkonadiServer...";
