e61d2d03e9fc9d8d353c6906984cf7c6b1f4f2ea
Daniel Vrátil <dvratil@kde.org>
2016-10-13 21:35:46 +0200

    Fix crash when Scotland is in the wrong thread

--- src/core/braveheart.cpp
+++ src/core/braveheart.cpp
@@ -33,24 +33,36 @@ namespace Akonadi
 
 class Braveheart
 {
+private:
+    static void sonOfScotland()
+    {
+        Q_ASSERT(qApp->thread() == QThread::currentThread());
+
+        if (!qApp->property("__Akonadi__Braveheart").isNull()) {
+            // One Scottish warrior is enough....
+            return;
+        }
+        auto freedom = new QTimer(qApp);
+        QObject::connect(freedom, &QTimer::timeout,
+                         freedom, []() {
+                            // They may take our lives, but they will never
+                            // take our memory!
+                            malloc_trim(50 * 1024 * 1024);
+                        });
+        // Fight for freedom every 15 minutes
+        freedom->start(15 * 60 * 1000);
+        qApp->setProperty("__Akonadi__Braveheart", true);
+    };
+
 public:
     explicit Braveheart()
     {
         qAddPreRoutine([]() {
-            if (!qApp->property("__Akonadi__Braveheart").isNull()) {
-                // One Scottish warrior is enough....
-                return;
+            if (qApp->thread() != QThread::currentThread()) {
+                QTimer::singleShot(0, qApp, sonOfScotland);
+            } else {
+                sonOfScotland();
             }
-            auto freedom = new QTimer();
-            QObject::connect(freedom, &QTimer::timeout,
-                            freedom, []() {
-                                // They may take our lives, but they will never
-                                // take our memory!
-                                malloc_trim(50 * 1024 * 1024);
-                            });
-            // Fight for freedom every 15 minutes
-            freedom->start(15 * 60 * 1000);
-            qApp->setProperty("__Akonadi__Braveheart", true);
         });
     }
 };
