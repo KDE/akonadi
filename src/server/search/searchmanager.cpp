KDE/akonadi

commit d54d1f241f1a421330efbf2f71efe4d1acd18f04

Search: match all results against DB first to exclude invalid results
In some cases the search backend can return results that actually
no longer exist in Akonadi DB (for example because the indexer is
busy indexing something else while an Item was removed, which triggered
search update), or because it went out-of-sync with the DB due to
a bug or manual cleanup. This lead to a lots of (albeit harmless)
foreign key constraint errors as we tried to insert items into
CollectionPimItemRelation table which did not exist in PimItemTable.
To prevent this we first match the results against PimItemTable to
get only the existing Items (we need them anyway for change ntf)
and then insert only those into the CollectionPimItemRelation table.

This does not fix problems with search updates unfortunately.

CCBUG: 353675

src/server/search/searchmanager.cpp
@@ -427,35 +427,54 @@ void SearchManager::searchUpdateResultsAvailable(const QSet<qint64> &results)
     qCDebug(AKONADISERVER_LOG) << "Got" << newMatches.count() << "results, out of which" << existingMatches.count() << "are already in the collection";
 
     newMatches = newMatches - existingMatches;
+    if (newMatches.isEmpty()) {
+        qCDebug(AKONADISERVER_LOG) << "Added results: 0 (fast path)";
+        return;
+    }
 
     const bool existingTransaction = DataStore::self()->inTransaction();
     if (!existingTransaction) {
         DataStore::self()->beginTransaction();
     }
 
+    // First query all the IDs we got from search plugin/agent against the DB.
+    // This will remove IDs that no longer exist in the DB.
     QVariantList newMatchesVariant;
     newMatchesVariant.reserve(newMatches.count());
     Q_FOREACH (qint64 id, newMatches) {
         newMatchesVariant << id;
-        Collection::addPimItem(collection.id(), id);
     }
 
-    qCDebug(AKONADISERVER_LOG) << "Added" << newMatches.count();
+    SelectQueryBuilder<PimItem> qb;
+    qb.addValueCondition(PimItem::idFullColumnName(), Query::In, newMatchesVariant);
+    if (!qb.exec()) {
+        return;
+    }
+
+    const auto items = qb.result();
+    if (items.count() != newMatches.count()) {
+        qCDebug(AKONADISERVER_LOG) << "Search backend returned" << (newMatches.count() - items.count()) << "results that no longer exist in Akonadi.";
+        qCDebug(AKONADISERVER_LOG) << "Please reindex collection" << collection.id();
+        // TODO: Request the reindexing directly from here
+    }
+
+    if (items.isEmpty()) {
+        qCDebug(AKONADISERVER_LOG) << "Added results: 0";
+        return;
+    }
+
+    for (const auto item : items) {
+        Collection::addPimItem(collection.id(), item.id());
+    }
 
     if (!existingTransaction && !DataStore::self()->commitTransaction()) {
         qCDebug(AKONADISERVER_LOG) << "Failed to commit transaction";
         return;
     }
 
-    if (!newMatchesVariant.isEmpty()) {
-        SelectQueryBuilder<PimItem> qb;
-        qb.addValueCondition(PimItem::idFullColumnName(), Query::In, newMatchesVariant);
-        if (!qb.exec()) {
-            return ;
-        }
-        const QVector<PimItem> newItems = qb.result();
-        DataStore::self()->notificationCollector()->itemsLinked(newItems, collection);
-        // Force collector to dispatch the notification now
-        DataStore::self()->notificationCollector()->dispatchNotifications();
-    }
+    DataStore::self()->notificationCollector()->itemsLinked(items, collection);
+    // Force collector to dispatch the notification now
+    DataStore::self()->notificationCollector()->dispatchNotifications();
+
+    qCDebug(AKONADISERVER_LOG) << "Added results:" << items.count();
 }

commit 6adadc171c24cdf53258939034fc213418d9e9b7

Fix database deadlock introduced in commit d54d1f241
The early-return optimization in SearchManager::searchUpdateResultsAvailable()
did not commit or rollback the transaction that was opened in that method.
If the transaction acquired an exclusive lock it would block all other DB
connections from acquiring exclusive transactions on that table forever.
This only affects SQLite, MySQL/PSQL seems to be able to deal with it
thanks to row locking.

src/server/search/searchmanager.cpp
@@ -365,7 +365,7 @@ void SearchManager::updateSearchImpl(const Collection &collection, QSemaphore *c
         return;
     }
 
-    DataStore::self()->beginTransaction();
+    Transaction transaction(DataStore::self());
 
     // Unlink all items that were not in search results from the collection
     QVariantList toRemove;
@@ -377,7 +377,7 @@ void SearchManager::updateSearchImpl(const Collection &collection, QSemaphore *c
         }
     }
 
-    if (!DataStore::self()->commitTransaction()) {
+    if (!transaction.commit()) {
         wakeUpCaller(cond);
         return;
     }
@@ -432,10 +432,7 @@ void SearchManager::searchUpdateResultsAvailable(const QSet<qint64> &results)
         return;
     }
 
-    const bool existingTransaction = DataStore::self()->inTransaction();
-    if (!existingTransaction) {
-        DataStore::self()->beginTransaction();
-    }
+    Transaction transaction(DataStore::self(), !DataStore::self()->inTransaction());
 
     // First query all the IDs we got from search plugin/agent against the DB.
     // This will remove IDs that no longer exist in the DB.
@@ -459,7 +456,7 @@ void SearchManager::searchUpdateResultsAvailable(const QSet<qint64> &results)
     }
 
     if (items.isEmpty()) {
-        qCDebug(AKONADISERVER_LOG) << "Added results: 0";
+        qCDebug(AKONADISERVER_LOG) << "Added results: 0 (no existing result)";
         return;
     }
 
@@ -467,7 +464,7 @@ void SearchManager::searchUpdateResultsAvailable(const QSet<qint64> &results)
         Collection::addPimItem(collection.id(), item.id());
     }
 
-    if (!existingTransaction && !DataStore::self()->commitTransaction()) {
+    if (!transaction.commit()) {
         qCDebug(AKONADISERVER_LOG) << "Failed to commit transaction";
         return;
     }