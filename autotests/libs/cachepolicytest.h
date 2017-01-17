25537a3df75175c784a1bec6dbf91046ece22cc9
Montel Laurent <montel@kde.org>
2017-01-17 21:57:36 +0100

    Start to create autotest for this class

---
+++ autotests/libs/cachepolicytest.h
@@ -0,0 +1,35 @@
+/*
+    Copyright (c) 2017 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef CACHEPOLICYTEST_H
+#define CACHEPOLICYTEST_H
+
+#include <QObject>
+
+class CachePolicyTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CachePolicyTest(QObject *parent = nullptr);
+    ~CachePolicyTest();
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+};
+
+#endif // CACHEPOLICYTEST_H
