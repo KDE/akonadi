d24149ff79d8617b46f9bc7fe6add648ba83e84c
Montel Laurent <montel@kde.org>
2017-04-20 20:28:44 +0200

    Use Q_FALLTHROUGH here

--- autotests/libs/testrunner/setup.cpp
+++ autotests/libs/testrunner/setup.cpp
@@ -268,6 +268,9 @@ void SetupTest::shutdown()
     case Akonadi::ServerManager::NotRunning:
     case Akonadi::ServerManager::Broken:
         shutdownHarder();
+#if QT_VERSION >= QT_VERSION_CHECK(5,8,0)
+        Q_FALLTHROUGH();
+#endif
     case Akonadi::ServerManager::Stopping:
         // safety timeout
         QTimer::singleShot(30 * 1000, this, SLOT(shutdownHarder()));
