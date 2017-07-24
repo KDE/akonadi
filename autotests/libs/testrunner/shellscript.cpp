eea4afefa57f1c8269ef7185568e324b562b315d
David Faure <faure@kde.org>
2017-07-25 00:55:51 +0200

    _old_XDG_CONFIG_HOME=XDG_CONFIG_HOME is funny but not very useful...

--- autotests/libs/testrunner/shellscript.cpp
+++ autotests/libs/testrunner/shellscript.cpp
@@ -31,7 +31,7 @@ ShellScript::ShellScript()
 void ShellScript::writeEnvironmentVariables()
 {
     foreach (const EnvVar &envvar, mEnvVars) {
-        mScript += QLatin1String("_old_") + QLatin1String(envvar.first) + QLatin1String("=") + QLatin1String(envvar.first) + QLatin1String("\n");
+        mScript += QLatin1String("_old_") + QLatin1String(envvar.first) + QLatin1String("=$") + QLatin1String(envvar.first) + QLatin1String("\n");
         mScript.append(QLatin1String(envvar.first));
         mScript.append(QLatin1String("=\""));
         QString value = QString::fromLocal8Bit(envvar.second);
