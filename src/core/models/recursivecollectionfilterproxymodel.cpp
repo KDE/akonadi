668632a17c551e80b67de36b7d5eb92f17d6abd6
Laurent Montel <montel@kde.org>
2018-01-01 11:50:22 +0100

    GIT_SILENT: Happy new year!

--- src/core/models/recursivecollectionfilterproxymodel.cpp
+++ src/core/models/recursivecollectionfilterproxymodel.cpp
@@ -1,6 +1,6 @@
 /*
     Copyright (c) 2009 Stephen Kelly <steveire@gmail.com>
-    Copyright (C) 2012-2017 Laurent Montel <montel@kde.org>
+    Copyright (C) 2012-2018 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
