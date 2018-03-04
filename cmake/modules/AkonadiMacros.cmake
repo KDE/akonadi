9e7ccc2f56c038fb0d31a24dd9fee579f38a7fee
Daniel Vrátil <dvratil@kde.org>
2018-03-04 22:43:08 +0100

    More xsltproc argument quotes

--- cmake/modules/AkonadiMacros.cmake
+++ cmake/modules/AkonadiMacros.cmake
@@ -26,19 +26,19 @@ add_custom_command(
   OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.h
          ${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.cpp
   COMMAND ${XSLTPROC_EXECUTABLE}
-          --output ${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.h
+          --output "${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.h"
           --stringparam code header
           --stringparam className ${_className}
           --stringparam fileName ${_fileBaseName}
-          ${Akonadi_SOURCE_DIR}/src/server/storage/schema.xsl
-          ${_schemaXml}
+          "${Akonadi_SOURCE_DIR}/src/server/storage/schema.xsl"
+          "${_schemaXml}"
   COMMAND ${XSLTPROC_EXECUTABLE}
-          --output ${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.cpp
+          --output "${CMAKE_CURRENT_BINARY_DIR}/${_fileBaseName}.cpp"
           --stringparam code source
           --stringparam className ${_className}
           --stringparam fileName ${_fileBaseName}
-          ${Akonadi_SOURCE_DIR}/src/server/storage/schema.xsl
-          ${_schemaXml}
+          "${Akonadi_SOURCE_DIR}/src/server/storage/schema.xsl"
+          "${_schemaXml}"
   DEPENDS ${Akonadi_SOURCE_DIR}/src/server/storage/schema.xsl
           ${Akonadi_SOURCE_DIR}/src/server/storage/schema-header.xsl
           ${Akonadi_SOURCE_DIR}/src/server/storage/schema-source.xsl
