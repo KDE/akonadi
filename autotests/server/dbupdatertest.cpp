62e0743b0d52b77295861d09ed5e3849ed73fd6d
Daniel Vrátil <dvratil@kde.org>
2018-08-14 10:58:00 +0200

    More test build fixes for CI

--- autotests/server/dbupdatertest.cpp
+++ autotests/server/dbupdatertest.cpp
@@ -75,7 +75,7 @@ void DbUpdaterTest::testPsqlUpdateStatements()
         for (const auto expected : expectedSets) {
             QVERIFY(updateSets.contains(expected));
         }
-        QCOMPARE(updateSets.count(), expectedSets.size());
+        QCOMPARE(std::size_t(updateSets.count()), expectedSets.size());
     }
 
     {
@@ -88,7 +88,7 @@ void DbUpdaterTest::testPsqlUpdateStatements()
         for (const auto expected : expectedSets) {
             QVERIFY(updateSets.contains(expected));
         }
-        QCOMPARE(updateSets.count(), expectedSets.size());
+        QCOMPARE(std::size_t(updateSets.count()), expectedSets.size());
 
         QCOMPARE(updateSets.value(14).statements.count(), 2);
         QCOMPARE(updateSets.value(16).statements.count(), 11);
