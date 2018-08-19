ff09451c85d59c9deceebfcc0524bcba0a52c6d1
Daniel Vrátil <dvratil@kde.org>
2018-08-19 13:01:40 +0200

    Fix typos in API docs

--- src/core/agentconfigurationbase.h
+++ src/core/agentconfigurationbase.h
@@ -66,7 +66,7 @@ namespace Akonadi {
  * {
  *     "X-Akonadi-PluginType": "AgentConfig",
  *     "X-Akonadi-Library": "exampleresourceconfig",
- *     "X-Akonadi-AgentConfig-Type"": "akonadi_example_resource"
+ *     "X-Akonadi-AgentConfig-Type": "akonadi_example_resource"
  * }
  * @endcode
  *
