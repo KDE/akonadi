KDE/akonadi

commit 9ea06c01ba5682e8c7a3bc7c17e2fdf8b066693e

Clean up TagFetchScope handling on client and server side
The fetch scope on the client side was using some hacks to list the
parts (GID, RID, ...) that should be retrieved and on the Item
retrieval part this was coupled with ItemFetchScope. This change
adds standalone TagFetchScope to FetchItemsCommand as well as
ItemFetchJob and similar APIs, allowing users to fully customize
the retrieval.

src/server/aggregatedfetchscope.cpp
@@ -100,6 +100,8 @@ public:
     QSet<QByteArray> attrs;
     QHash<QByteArray, int> attrsCount;
     int fetchIdOnly = 0;
+    int fetchRemoteId = 0;
+    int fetchAllAttributes = 0;
 };
 
 class AggregatedItemFetchScopePrivate : public AggregatedFetchScopePrivate
@@ -228,9 +230,6 @@ void AggregatedItemFetchScope::apply(const Protocol::ItemFetchScope &oldScope,
     if (newParts != oldParts) {
         d->applySet(oldParts, newParts, d->parts, d->partsCount);
     }
-    if (newScope.tagFetchScope() != oldScope.tagFetchScope()) {
-        d->applySet(oldScope.tagFetchScope(), newScope.tagFetchScope(), d->tags, d->tagsCount);
-    }
     if (newScope.ancestorDepth() != oldScope.ancestorDepth()) {
         updateAncestorDepth(oldScope.ancestorDepth(), newScope.ancestorDepth());
     }
@@ -264,9 +263,6 @@ void AggregatedItemFetchScope::apply(const Protocol::ItemFetchScope &oldScope,
     if (newScope.fetchGID() != oldScope.fetchGID()) {
         setFetchGID(newScope.fetchGID());
     }
-    if (newScope.fetchTags() != oldScope.fetchTags()) {
-        setFetchTags(newScope.fetchTags());
-    }
     if (newScope.fetchRelations() != oldScope.fetchRelations()) {
         setFetchRelations(newScope.fetchRelations());
     }
@@ -286,7 +282,6 @@ ItemFetchScope AggregatedItemFetchScope::toFetchScope() const
 
     d->mCachedScope = ItemFetchScope();
     d->mCachedScope.setRequestedParts(setToVector(d->parts));
-    d->mCachedScope.setTagFetchScope(d->tags);
     d->mCachedScope.setAncestorDepth(ancestorDepth());
 
     d->mCachedScope.setFetch(ItemFetchScope::CacheOnly, cacheOnly());
@@ -299,7 +294,6 @@ ItemFetchScope AggregatedItemFetchScope::toFetchScope() const
     d->mCachedScope.setFetch(ItemFetchScope::Flags, fetchFlags());
     d->mCachedScope.setFetch(ItemFetchScope::RemoteID, fetchRemoteId());
     d->mCachedScope.setFetch(ItemFetchScope::GID, fetchGID());
-    d->mCachedScope.setFetch(ItemFetchScope::Tags, fetchTags());
     d->mCachedScope.setFetch(ItemFetchScope::Relations, fetchRelations());
     d->mCachedScope.setFetch(ItemFetchScope::VirtReferences, fetchVirtualReferences());
     d->mCachedScopeValid = true;
@@ -322,24 +316,6 @@ void AggregatedItemFetchScope::removeRequestedPart(const QByteArray &part)
 {
     LOCKED_D(AggregatedItemFetchScope)
     d->removeFromSet(part, d->parts, d->partsCount);
-}
-
-QSet<QByteArray> AggregatedItemFetchScope::tagFetchScope() const
-{
-    LOCKED_D(const AggregatedItemFetchScope)
-    return d->tags;
-}
-
-void AggregatedItemFetchScope::addTag(const QByteArray &tag)
-{
-    LOCKED_D(AggregatedItemFetchScope)
-    d->addToSet(tag, d->tags, d->tagsCount);
-}
-
-void AggregatedItemFetchScope::removeTag(const QByteArray &tag)
-{
-    LOCKED_D(AggregatedItemFetchScope)
-    d->removeFromSet(tag, d->tags, d->tagsCount);
 }
 
 ItemFetchScope::AncestorDepth AggregatedItemFetchScope::ancestorDepth() const
@@ -499,19 +475,6 @@ void AggregatedItemFetchScope::setFetchGID(bool fetchGid)
     d->updateBool(fetchGid, d->fetchGID);
 }
 
-bool AggregatedItemFetchScope::fetchTags() const
-{
-    LOCKED_D(const AggregatedItemFetchScope)
-    // Aggregation: return true if there's at least one subscriber who wants tags
-    return d->fetchTags > 0;
-}
-
-void AggregatedItemFetchScope::setFetchTags(bool fetchTags)
-{
-    LOCKED_D(AggregatedItemFetchScope)
-    d->updateBool(fetchTags, d->fetchTags);
-}
-
 bool AggregatedItemFetchScope::fetchRelations() const
 {
     LOCKED_D(const AggregatedItemFetchScope)
@@ -561,11 +524,26 @@ void AggregatedTagFetchScope::apply(const Protocol::TagFetchScope &oldScope,
     if (newScope.fetchIdOnly() != oldScope.fetchIdOnly()) {
         setFetchIdOnly(newScope.fetchIdOnly());
     }
+    if (newScope.fetchRemoteID() != oldScope.fetchRemoteID()) {
+        setFetchRemoteId(newScope.fetchRemoteID());
+    }
+    if (newScope.fetchAllAttributes() != oldScope.fetchAllAttributes()) {
+        setFetchAllAttributes(newScope.fetchAllAttributes());
+    }
     if (newScope.attributes() != oldScope.attributes()) {
         d->applySet(oldScope.attributes(), newScope.attributes(), d->attrs, d->attrsCount);
     }
 }
 
+Protocol::TagFetchScope AggregatedTagFetchScope::toFetchScope() const
+{
+    Protocol::TagFetchScope tfs;
+    tfs.setFetchIdOnly(fetchIdOnly());
+    tfs.setFetchRemoteID(fetchRemoteId());
+    tfs.setFetchAllAttributes(fetchAllAttributes());
+    tfs.setAttributes(attributes());
+    return tfs;
+}
 
 bool AggregatedTagFetchScope::fetchIdOnly() const
 {
@@ -579,6 +557,30 @@ void AggregatedTagFetchScope::setFetchIdOnly(bool fetchIdOnly)
 {
     LOCKED_D(AggregatedTagFetchScope)
     d->updateBool(fetchIdOnly, d->fetchIdOnly);
+}
+
+bool AggregatedTagFetchScope::fetchRemoteId() const
+{
+    LOCKED_D(const AggregatedTagFetchScope)
+    return d->fetchRemoteId > 0;
+}
+
+void AggregatedTagFetchScope::setFetchRemoteId(bool fetchRemoteId)
+{
+    LOCKED_D(AggregatedTagFetchScope)
+    d->updateBool(fetchRemoteId, d->fetchRemoteId);
+}
+
+bool AggregatedTagFetchScope::fetchAllAttributes() const
+{
+    LOCKED_D(const AggregatedTagFetchScope)
+    return d->fetchAllAttributes > 0;
+}
+
+void AggregatedTagFetchScope::setFetchAllAttributes(bool fetchAllAttributes)
+{
+    LOCKED_D(AggregatedTagFetchScope)
+    d->updateBool(fetchAllAttributes, d->fetchAllAttributes);
 }
 
 QSet<QByteArray> AggregatedTagFetchScope::attributes() const

src/server/aggregatedfetchscope.h
@@ -69,10 +69,6 @@ public:
     void addRequestedPart(const QByteArray &part);
     void removeRequestedPart(const QByteArray &part);
 
-    QSet<QByteArray> tagFetchScope() const;
-    void addTag(const QByteArray &tag);
-    void removeTag(const QByteArray &tag);
-
     Protocol::ItemFetchScope::AncestorDepth ancestorDepth() const;
     void updateAncestorDepth(Protocol::ItemFetchScope::AncestorDepth oldDepth,
                              Protocol::ItemFetchScope::AncestorDepth newDepth);
@@ -97,8 +93,6 @@ public:
     void setFetchRemoteId(bool fetchRemoteId);
     bool fetchGID() const;
     void setFetchGID(bool fetchGid);
-    bool fetchTags() const;
-    void setFetchTags(bool fetchTags);
     bool fetchRelations() const;
     void setFetchRelations(bool fetchRelations);
     bool fetchVirtualReferences() const;
@@ -119,6 +113,7 @@ public:
 
     void apply(const Protocol::TagFetchScope &oldScope,
                const Protocol::TagFetchScope &newScope);
+    Protocol::TagFetchScope toFetchScope() const;
 
     QSet<QByteArray> attributes() const;
     void addAttribute(const QByteArray &attribute);
@@ -127,6 +122,11 @@ public:
     bool fetchIdOnly() const;
     void setFetchIdOnly(bool fetchIdOnly);
 
+    bool fetchRemoteId() const;
+    void setFetchRemoteId(bool fetchRemoteId);
+
+    bool fetchAllAttributes() const;
+    void setFetchAllAttributes(bool fetchAllAttributes);
 private:
     AggregatedTagFetchScopePrivate * const d_ptr;
     Q_DECLARE_PRIVATE(AggregatedTagFetchScope)

CMakeLists.txt
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.0)
 
-set(PIM_VERSION "5.9.42")
+set(PIM_VERSION "5.9.93")
 project(Akonadi VERSION ${PIM_VERSION})
 
 set(CMAKE_CXX_STANDARD 14)

autotests/libs/tagsynctest.cpp
@@ -27,6 +27,7 @@
 #include <tagcreatejob.h>
 #include <tag.h>
 #include <tagsync.h>
+#include <tagfetchscope.h>
 #include <resourceselectjob_p.h>
 #include <itemcreatejob.h>
 #include <itemfetchjob.h>
@@ -53,6 +54,7 @@ private Q_SLOTS:
     Tag::List getTags()
     {
         TagFetchJob *fetchJob = new TagFetchJob();
+        fetchJob->fetchScope().setFetchRemoteId(true);
         bool ret = fetchJob->exec();
         Q_ASSERT(ret);
         return fetchJob->tags();
@@ -123,7 +125,7 @@ private Q_SLOTS:
         }
 
         QHash<QString, Item::List> tagMembers;
-        tagMembers.insert(QString::fromLatin1(tag1.remoteId()), Item::List() << item1);
+        tagMembers.insert(QString::fromLatin1(tag1.remoteId()), { item1 });
 
         TagSync *syncer = new TagSync(this);
         syncer->setFullTagList(remoteTags);
@@ -138,6 +140,8 @@ private Q_SLOTS:
         tag1 = resultTags.first();
 
         ItemFetchJob *fetchJob = new ItemFetchJob(tag1);
+        fetchJob->fetchScope().setFetchTags(true);
+        fetchJob->fetchScope().tagFetchScope().setFetchRemoteId(true);
         AKVERIFYEXEC(fetchJob);
         QCOMPARE(fetchJob->items().count(), tagMembers.value(QString::fromLatin1(tag1.remoteId())).count());
         QCOMPARE(fetchJob->items(), tagMembers.value(QString::fromLatin1(tag1.remoteId())));

autotests/libs/tagtest.cpp
@@ -161,6 +161,7 @@ void TagTest::testRID()
 
     {
         TagFetchJob *fetchJob = new TagFetchJob(this);
+        fetchJob->fetchScope().setFetchRemoteId(true);
         AKVERIFYEXEC(fetchJob);
         QCOMPARE(fetchJob->tags().size(), 1);
         QCOMPARE(fetchJob->tags().first().gid(), QByteArray("gid"));
@@ -195,10 +196,8 @@ void TagTest::testRIDIsolation()
     qint64 tagId;
     {
         TagFetchJob *fetchJob = new TagFetchJob(this);
+        fetchJob->fetchScope().setFetchRemoteId(true);
         AKVERIFYEXEC(fetchJob);
-        Q_FOREACH (const Tag &tag, fetchJob->tags()) {
-            qDebug() << tag.gid();
-        }
         QCOMPARE(fetchJob->tags().count(), 1);
         QCOMPARE(fetchJob->tags().first().gid(), QByteArray("gid"));
         QCOMPARE(fetchJob->tags().first().type(), QByteArray("mytype"));
@@ -219,6 +218,7 @@ void TagTest::testRIDIsolation()
 
     {
         TagFetchJob *fetchJob = new TagFetchJob(this);
+        fetchJob->fetchScope().setFetchRemoteId(true);
         AKVERIFYEXEC(fetchJob);
         QCOMPARE(fetchJob->tags().count(), 1);
         QCOMPARE(fetchJob->tags().first().gid(), QByteArray("gid"));

autotests/private/protocoltest.cpp
@@ -233,7 +233,6 @@ void ProtocolTest::testFetchScope()
 
     in.setRequestedParts(requestedParts);
     in.setChangedSince(QDateTime(QDate(2015, 8, 10), QTime(23, 52, 20), Qt::UTC));
-    in.setTagFetchScope({ "TAGID" });
     in.setAncestorDepth(ItemFetchScope::AllAncestors);
     in.setFetch(ItemFetchScope::CacheOnly);
     in.setFetch(ItemFetchScope::CheckCachedPayloadPartsOnly);
@@ -254,7 +253,6 @@ void ProtocolTest::testFetchScope()
     QCOMPARE(out.requestedParts(), expectedParts);
     QCOMPARE(out.requestedPayloads(), expectedPayloads);
     QCOMPARE(out.changedSince(), QDateTime(QDate(2015, 8, 10), QTime(23, 52, 20), Qt::UTC));
-    QCOMPARE(out.tagFetchScope(), QSet<QByteArray>{ "TAGID" });
     QCOMPARE(out.ancestorDepth(), ItemFetchScope::AllAncestors);
     QCOMPARE(out.fetch(ItemFetchScope::None), false);
     QCOMPARE(out.cacheOnly(), true);

autotests/server/fetchhandlertest.cpp
@@ -63,7 +63,9 @@ public:
     Protocol::FetchItemsCommandPtr createCommand(const Scope &scope, const Protocol::ScopeContext &ctx = Protocol::ScopeContext())
     {
         auto cmd = Protocol::FetchItemsCommandPtr::create(scope, ctx);
-        cmd->fetchScope().setFetch(Protocol::ItemFetchScope::IgnoreErrors);
+        auto fetchScope = cmd->itemFetchScope();
+        fetchScope.setFetch(Protocol::ItemFetchScope::IgnoreErrors);
+        cmd->setItemFetchScope(fetchScope);
         return cmd;
     }
 

src/core/jobs/itemfetchjob.cpp
@@ -25,6 +25,7 @@
 #include "job_p.h"
 #include "protocolhelper_p.h"
 #include "session_p.h"
+#include "tagfetchscope.h"
 #include "private/protocol_p.h"
 
 
@@ -195,7 +196,8 @@ void ItemFetchJob::doStart()
         d->sendCommand(Protocol::FetchItemsCommandPtr::create(
                            d->mRequestedItems.isEmpty() ? Scope() : ProtocolHelper::entitySetToScope(d->mRequestedItems),
                            ProtocolHelper::commandContextToProtocol(d->mCollection, d->mCurrentTag, d->mRequestedItems),
-                           ProtocolHelper::itemFetchScopeToProtocol(d->mFetchScope)));
+                           ProtocolHelper::itemFetchScopeToProtocol(d->mFetchScope),
+                           ProtocolHelper::tagFetchScopeToProtocol(d->mFetchScope.tagFetchScope())));
     } catch (const Akonadi::Exception &e) {
         setError(Job::Unknown);
         setErrorText(QString::fromUtf8(e.what()));

commit 542d234d9ce696d31af6293913ef07294572602c

Fix regression in tag fetching for notifications
Commit 9ea06c01ba introduced some clean-up in tag
fetching. Unfortunately with it came a regression which caused tags
not to be fetched as part of notifications - no matter how the fetch
scope is configured.

The problem lies in the fact that the FetchHelper class, which
populates items based on the configured fetch scope still uses the
Tags flag in the item fetch scope to determine if tags should be
fetched at all and only if this flag is set the tag fetch scope is
examined to determine what tag information to include.

On the notification side however support for handling this flag in the
AggregatedItemFetchScope was removed, which causes the flag never to
be set for the item fetch scope, which in turn means that no tags will
ever be fetched for notifications regardless of tag fetch scope
setting.

This commit brings back handling of the Tags flag in the item fetch
scope thereby partially reverting commit 9ea06c01ba.

Reviewers: #kde_pim

Reviewed By: dvratil

Differential Revision: https://phabricator.kde.org/D16512

src/server/aggregatedfetchscope.cpp
@@ -263,6 +263,9 @@ void AggregatedItemFetchScope::apply(const Protocol::ItemFetchScope &oldScope,
     if (newScope.fetchGID() != oldScope.fetchGID()) {
         setFetchGID(newScope.fetchGID());
     }
+    if (newScope.fetchTags() != oldScope.fetchTags()) {
+        setFetchTags(newScope.fetchTags());
+    }
     if (newScope.fetchRelations() != oldScope.fetchRelations()) {
         setFetchRelations(newScope.fetchRelations());
     }
@@ -294,6 +297,7 @@ ItemFetchScope AggregatedItemFetchScope::toFetchScope() const
     d->mCachedScope.setFetch(ItemFetchScope::Flags, fetchFlags());
     d->mCachedScope.setFetch(ItemFetchScope::RemoteID, fetchRemoteId());
     d->mCachedScope.setFetch(ItemFetchScope::GID, fetchGID());
+    d->mCachedScope.setFetch(ItemFetchScope::Tags, fetchTags());
     d->mCachedScope.setFetch(ItemFetchScope::Relations, fetchRelations());
     d->mCachedScope.setFetch(ItemFetchScope::VirtReferences, fetchVirtualReferences());
     d->mCachedScopeValid = true;
@@ -473,6 +477,19 @@ void AggregatedItemFetchScope::setFetchGID(bool fetchGid)
 {
     LOCKED_D(AggregatedItemFetchScope)
     d->updateBool(fetchGid, d->fetchGID);
+}
+
+bool AggregatedItemFetchScope::fetchTags() const
+{
+    LOCKED_D(const AggregatedItemFetchScope)
+    // Aggregation: return true if there's at least one subscriber who wants tags
+    return d->fetchTags > 0;
+}
+
+void AggregatedItemFetchScope::setFetchTags(bool fetchTags)
+{
+    LOCKED_D(AggregatedItemFetchScope)
+    d->updateBool(fetchTags, d->fetchTags);
 }
 
 bool AggregatedItemFetchScope::fetchRelations() const

src/server/aggregatedfetchscope.h
@@ -93,6 +93,8 @@ public:
     void setFetchRemoteId(bool fetchRemoteId);
     bool fetchGID() const;
     void setFetchGID(bool fetchGid);
+    bool fetchTags() const;
+    void setFetchTags(bool fetchTags);
     bool fetchRelations() const;
     void setFetchRelations(bool fetchRelations);
     bool fetchVirtualReferences() const;