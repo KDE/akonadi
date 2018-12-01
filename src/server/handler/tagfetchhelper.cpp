KDE/akonadi

commit 9d83f0752828942789535b1d0dc0ddc3c8b479bd

Fix KOrg category color customization crash
Summary:
Use default value for NULL columns.

Without this patch, possibly different default values are used for
entity construction (default from akonadidb.xml) and when reading a row
from the database (default-constructed), resulting in inconsistencies.
For example, when modifying a Tag, the resulting Tag modify notification
indicates that the new parentId of the modified Tag is 0
(default-constructed qint64), whereas Tags without parents are
initialized with parentId -1. This patch fixes these inconsistencies by
considering the "default" value from akonadidb.xml for row-to-entity
conversion, too.

Set default Tag::parentId to -1.

A parentless Tag is initialized with parentId -1, so we should also
report that when reading a Tag from the database where the parentId
field is NULL.

BUG: 398459
FIXED-IN: 5.9.3

Test Plan:
KOrganizer stops crashing on category color customization
with this patch. More detailed test plan suggestions welcome.

Reviewers: #kde_pim, dvratil

Reviewed By: #kde_pim, dvratil

Subscribers: dvratil, kde-pim

Tags: #kde_pim

Differential Revision: https://phabricator.kde.org/D16051

src/server/storage/akonadidb.xml
@@ -67,7 +67,7 @@
     <comment>Contains the schema version of the database.</comment>
     <column name="version" type="int" default="0" allowNull="false"/>
     <column name="generation" type="int" default="0" allowNull="false" />
-    <data columns="version" values="35"/>
+    <data columns="version" values="36"/>
   </table>
 
   <table name="Resource">
@@ -199,7 +199,7 @@
   <table name="Tag">
     <column name="id" type="qint64" allowNull="false" isAutoIncrement="true" isPrimaryKey="true"/>
     <column name="gid" type="QString" allowNull="false"/>
-    <column name="parentId" type="qint64" refTable="Tag" refColumn="id" allowNull="true" methodName="parent"/>
+    <column name="parentId" type="qint64" refTable="Tag" refColumn="id" allowNull="true" methodName="parent" default="-1"/>
     <column name="typeId" type="qint64" refTable="TagType" refColumn="id" onDelete="Restrict" default="1"/>
   </table>
 

src/server/storage/entities.xsl
@@ -224,7 +224,7 @@ set<xsl:value-of select="$methodName"/>( <xsl:call-template name="argument"/> )
       </xsl:when>
       <xsl:otherwise>
         (qb.query().isNull(valueIndex)) ?
-        <xsl:call-template name="data-type"/>() :
+        <xsl:call-template name="data-type"/>(<xsl:value-of select="@default"/>) :
         <xsl:choose>
           <xsl:when test="starts-with(@type,'QString')">
           Utils::variantToString( qb.query().value( valueIndex ) )

commit 2e72fef182db526d2fd118767236b12e96adf35c

Fix creating new tags with null parent
This fixes a regression introduced in 9d83f0752 that set default
value for Tag.parentId column as -1, so the Entity logic code tries
to insert -1, even though there's no Tag with ID -1, which triggers
foreign key constraint violation. This roots in our inability to
express absence of a value by plain integer.

This change reverts the patch partially and instead workarounds it
by resetting the parentId to -1 when reading a NULL value from the
table.

src/server/handler/tagfetchhelper.cpp
@@ -139,7 +139,12 @@ bool TagFetchHelper::fetchTags()
         response.setId(tagId);
         if (!mFetchScope.fetchIdOnly()) {
             response.setGid(Utils::variantToByteArray(tagQuery.value(1)));
-            response.setParentId(tagQuery.value(2).toLongLong());
+            if (tagQuery.value(2).isNull()) {
+                // client indicates invalid or null parent as ID -1
+                response.setParentId(-1);
+            } else {
+                response.setParentId(tagQuery.value(2).toLongLong());
+            }
             response.setType(Utils::variantToByteArray(tagQuery.value(3)));
             if (mFetchScope.fetchRemoteID() && mConnection->context()->resource().isValid()) {
                 response.setRemoteId(Utils::variantToByteArray(tagQuery.value(4)));

src/server/storage/akonadidb.xml
@@ -67,7 +67,7 @@
     <comment>Contains the schema version of the database.</comment>
     <column name="version" type="int" default="0" allowNull="false"/>
     <column name="generation" type="int" default="0" allowNull="false" />
-    <data columns="version" values="38"/>
+    <data columns="version" values="39"/>
   </table>
 
   <table name="Resource">
@@ -203,7 +203,7 @@
   <table name="Tag">
     <column name="id" type="qint64" allowNull="false" isAutoIncrement="true" isPrimaryKey="true"/>
     <column name="gid" type="QString" allowNull="false"/>
-    <column name="parentId" type="qint64" refTable="Tag" refColumn="id" allowNull="true" methodName="parent" default="-1"/>
+    <column name="parentId" type="qint64" refTable="Tag" refColumn="id" allowNull="true" methodName="parent" />
     <column name="typeId" type="qint64" refTable="TagType" refColumn="id" onDelete="Restrict" default="1"/>
     <index name="parentIndex" columns="parentId" unique="false"/>
     <index name="typeIndex" columns="typeId" unique="false"/>

src/server/storage/dbupdate.xml
@@ -339,4 +339,11 @@
   <update version="37" abortOnFailure="true">
     <raw-sql backends="sqlite">UPDATE TagRemoteIdResourceRelationTable SET remoteId = printf('%s', remoteId)</raw-sql>
   </update>
+
+  <update version="39" abortOnFailure="false">
+    <raw-sql backends="mysql">ALTER TABLE TagTable MODIFY COLUMN parentId BIGINT(20);</raw-sql>
+    <raw-sql backends="psql">ALTER TABLE TagTable ALTER COLUMN parentId DROP DEFAULT;</raw-sql>
+    <!-- FIXME: SQLite doesn't allow for a straight-forward migration, but this has never reached
+         a release, so we are mostly OK here. //-->
+  </update>
 </updates>