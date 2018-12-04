a490a189e41a2fe79211084e95037ccc51e948d7
Daniel Vrátil <dvratil@kde.org>
2018-12-04 14:01:58 +0100

    Revert "One more try for MSVC"
    This reverts commit b25c808942ef35dbdef406c3429b05258ecc6d5d.

--- src/shared/akoptional.h
+++ src/shared/akoptional.h
@@ -23,7 +23,8 @@
 #ifdef __has_include
     #if __has_include(<optional>)
         #include <optional>
-    #elif __has_include(<experimental/optional>)
+    #endif
+    #if !defined(__cpp_lib_optional) && __has_include(<experimental/optional>)
         #include <experimental/optional>
         namespace std { using namespace experimental; }
     #else
