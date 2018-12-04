d7b50cc612929d197a14804960fd374763345124
Daniel Vrátil <dvratil@kde.org>
2018-12-04 12:46:55 +0100

    Try to fix build on MSVC

--- src/shared/akoptional.h
+++ src/shared/akoptional.h
@@ -21,7 +21,7 @@
 #define AKOPTIONAL_H
 
 #ifdef __has_include
-    #if __has_include(<optional>)
+    #if __has_include(<optional>) && !defined(_MSC_VER) // MSVC won't let us use <optional> in C++14 despite having it
         #include <optional>
     #endif
     #if !defined(__cpp_lib_optional) && __has_include(<experimental/optional>)
