KDE/akonadi

commit 3a062e6a47f292fd5da7b99552894ae6d6d4e323

Fix intermittent race in collectionattributetest
Summary:
Any CollectionModifyJob of a collection with attributes, would send the
full list of attributes, even if they hadn't changed. This leads to the
following problem:
* collectionattributetest creates a collection with an attribute
* it then proceeds to modify the attribute
* then the knut resource's collectionAdded() wants to add a
remoteid to that collection, so it creates a ModifyJob of its own
(and that's where the bug happens: it includes all attributes, with the
orig value).

Therefore the old value overwrites the new value again.

Fixed by only sending attributes if they have changed.

Test Plan: collectionattributetest in a loop, doesn't fail anymore

Reviewers: dvratil, vkrause

Reviewed By: dvratil

Subscribers: kde-pim

Tags: #kde_pim

Differential Revision: https://phabricator.kde.org/D19290

src/core/collection.h
@@ -549,6 +549,8 @@ private:
     friend class CollectionModifyJob;
     friend class ProtocolHelper;
 
+    void markAttributesChanged();
+
     //@cond PRIVATE
     QSharedDataPointer<CollectionPrivate> d_ptr;
     friend class CollectionPrivate;
@@ -566,6 +568,7 @@ inline T *Akonadi::Collection::attribute(Collection::CreateOption option)
     if (hasAttribute(dummy.type())) {
         T *attr = dynamic_cast<T *>(attribute(dummy.type()));
         if (attr) {
+            markAttributesChanged();
             return attr;
         }
         //Reuse 5250

src/core/collection.cpp
@@ -177,6 +177,7 @@ void Collection::addAttribute(Attribute *attr)
     }
     d_ptr->mAttributes.insert(attr->type(), attr);
     d_ptr->mDeletedAttributes.remove(attr->type());
+    d_ptr->attributesChanged = true;
 }
 
 void Collection::removeAttribute(const QByteArray &type)
@@ -459,4 +460,9 @@ void Collection::setKeepLocalChanges(const QSet<QByteArray> &parts)
 QSet<QByteArray> Collection::keepLocalChanges() const
 {
     return d_ptr->keepLocalChanges;
+}
+
+void Collection::markAttributesChanged()
+{
+    d_ptr->attributesChanged = true;
 }

src/core/collection_p.h
@@ -48,6 +48,7 @@ public:
         , referencedChanged(false)
         , contentTypesChanged(false)
         , cachePolicyChanged(false)
+        , attributesChanged(false)
         , isVirtual(false)
         , mId(id)
         , mParent(nullptr)
@@ -85,6 +86,7 @@ public:
         referenced = other.referenced;
         referencedChanged = other.referencedChanged;
         keepLocalChanges = other.keepLocalChanges;
+        attributesChanged = other.attributesChanged;
     }
 
     ~CollectionPrivate()
@@ -100,6 +102,7 @@ public:
         enabledChanged = false;
         listPreferenceChanged = false;
         referencedChanged = false;
+        attributesChanged = false;
         mDeletedAttributes.clear();
     }
 
@@ -121,6 +124,7 @@ public:
     bool referencedChanged: 1;
     bool contentTypesChanged: 1;
     bool cachePolicyChanged: 1;
+    bool attributesChanged : 1;
     bool isVirtual: 1;
     // 2 bytes padding here
 

src/core/jobs/collectionmodifyjob.cpp
@@ -100,7 +100,7 @@ void CollectionModifyJob::doStart()
     if (d->mCollection.d_ptr->referencedChanged) {
         cmd->setReferenced(d->mCollection.referenced());
     }
-    if (!d->mCollection.attributes().isEmpty()) {
+    if (d->mCollection.d_ptr->attributesChanged) {
         cmd->setAttributes(ProtocolHelper::attributesToProtocol(d->mCollection));
     }
     if (auto attr = d->mCollection.attribute<Akonadi::PersistentSearchAttribute>()) {

commit a5805b386ce5b56e9ae115d7b39dcc39f4eb3a66

Fix a regression when updating attributes
This fixes a regression introduced in 3a062e6a and 6054e42d where some
attributes were not sent to the Akonadi server in update job even though
they were modified. This was due to a bad API design which returns a
non-const pointer to an attribute from a const method, so callers sometimes
modify the returned attribute on a const object. Since the method itself
is const though, it did not mark the attribute as modified.

Proper fix is to introduce a purely const and non-const overloads for
the attribute accessors, unfortunatelly this requires fixing a lot of our code
in many places first to not abuse the non-constness of the returned
attribute.

Note that since the code is in an inlined method, all clients should be
recompiled.

CCMAIL: faure@kde.org

src/core/collection.h
@@ -584,14 +584,19 @@ inline T *Akonadi::Collection::attribute(Collection::CreateOption option)
 template <typename T>
 inline T *Akonadi::Collection::attribute() const
 {
-    const T dummy;
-    if (hasAttribute(dummy.type())) {
-        T *attr = dynamic_cast<T *>(attribute(dummy.type()));
+    const QByteArray type = T().type();
+    if (hasAttribute(type)) {
+        T *attr = dynamic_cast<T *>(attribute(type));
         if (attr) {
+            // FIXME: This method returns a non-const pointer, so callers may still modify the
+            // attribute. Unfortunately, just making this function return a const pointer and
+            // creating a non-const overload does not work, as many users of this function abuse the
+            // non-const pointer and modify the attribute even on a const object.
+            const_cast<Collection*>(this)->markAttributeModified(type);
             return attr;
         }
         //reuse 5250
-        qWarning() << "Found attribute of unknown type" << dummy.type()
+        qWarning() << "Found attribute of unknown type" << type
                    << ". Did you forget to call AttributeFactory::registerAttribute()?";
     }
 

src/core/item.cpp
@@ -254,7 +254,11 @@ void Akonadi::Item::clearAttributes()
 
 Attribute *Item::attribute(const QByteArray &type) const
 {
-    return ItemChangeLog::instance()->attributeStorage(d_ptr).attribute(type);
+    // FIXME: Createa a truly const and non-const overloads of this method
+    // so only non-const access marks the attribute as modified
+    auto &storage = ItemChangeLog::instance()->attributeStorage(d_ptr);
+    storage.markAttributeModified(type);
+    return attribute(type);
 }
 
 Collection &Item::parentCollection()

src/core/item.h
@@ -812,6 +812,7 @@ inline T *Item::attribute() const
     if (hasAttribute(dummy.type())) {
         T *attr = dynamic_cast<T *>(attribute(dummy.type()));
         if (attr) {
+            
             return attr;
         }
         //reuse 5250

src/core/tag.h
@@ -233,10 +233,13 @@ inline T *Tag::attribute(CreateOption option)
 template <typename T>
 inline T *Tag::attribute() const
 {
-    const T dummy;
-    if (hasAttribute(dummy.type())) {
-        T *attr = dynamic_cast<T *>(attribute(dummy.type()));
-        if (checkAttribute(attr, dummy.type())) {
+    const QByteArray type = T().type();
+    if (hasAttribute(type)) {
+        T *attr = dynamic_cast<T *>(attribute(type));
+        if (checkAttribute(attr, type)) {
+            // FIXME: Make this a truly const method so that callers may not modify
+            // the attribute returned from here.
+            const_cast<Tag*>(this)->markAttributeModified(type);
             return attr;
         }
     }