6426d256b9a26692263a7da553540c134a5a535f
Daniel Vrátil <dvratil@kde.org>
2019-05-31 13:18:43 +0200

    Introduce a simple scope guard helper AkScopeGuard
    Reviewers: #kde_pim, vkrause
    
    Reviewed By: #kde_pim, vkrause
    
    Subscribers: vkrause, kde-pim
    
    Tags: #kde_pim
    
    Differential Revision: https://phabricator.kde.org/D21454

---
+++ autotests/shared/akscopeguardtest.cpp
@@ -0,0 +1,98 @@
+/*
+    Copyright (C) 2019  Daniel Vrátil <dvratil@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include <QTest>
+#include <QObject>
+
+#include "shared/akscopeguard.h"
+
+using namespace Akonadi;
+
+class AkScopeGuardTest : public QObject
+{
+    Q_OBJECT
+
+private:
+    static void staticMethod() noexcept
+    {
+        Q_ASSERT(!mCalled);
+        mCalled = true;
+    }
+
+    void regularMethod() noexcept
+    {
+        Q_ASSERT(!mCalled);
+        mCalled = true;
+    }
+
+private Q_SLOTS:
+
+    void testLambda()
+    {
+        mCalled = false;
+        {
+            AkScopeGuard guard([&]() noexcept {
+                Q_ASSERT(!mCalled);
+                mCalled = true;
+            });
+        }
+        QVERIFY(mCalled);
+    }
+
+    void testStaticMethod()
+    {
+        mCalled = false;
+        {
+            AkScopeGuard guard(&AkScopeGuardTest::staticMethod);
+        }
+        QVERIFY(mCalled);
+    }
+
+    void testBindExpr()
+    {
+        mCalled = false;
+        {
+            AkScopeGuard guard(std::bind(&AkScopeGuardTest::regularMethod, this));
+        }
+        QVERIFY(mCalled);
+    }
+
+    void testStdFunction()
+    {
+        mCalled = false;
+        std::function<void() noexcept> func = [&]() noexcept {
+            Q_ASSERT(!mCalled);
+            mCalled = true;
+        };
+        {
+            AkScopeGuard guard(func);
+        }
+        QVERIFY(mCalled);
+    }
+
+private:
+    static bool mCalled;
+};
+
+bool AkScopeGuardTest::mCalled = false;
+
+QTEST_GUILESS_MAIN(AkScopeGuardTest)
+
+#include "akscopeguardtest.moc"
+
---
+++ src/shared/akscopeguard.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2019  Daniel Vrátil <dvratil@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef AKONADI_AKSCOPEGUARD_H_
+#define AKONADI_AKSCOPEGUARD_H_
+
+#include <functional>
+#include <type_traits>
+
+namespace Akonadi {
+
+class AkScopeGuard
+{
+public:
+    template<typename U>
+    AkScopeGuard(U &&fun)
+        : mFun(std::move(fun))
+    {}
+
+    AkScopeGuard(const AkScopeGuard &) = delete;
+    AkScopeGuard(AkScopeGuard &&) = default;
+    AkScopeGuard &operator=(const AkScopeGuard &) = delete;
+    AkScopeGuard &operator=(AkScopeGuard &&) = delete;
+
+    ~AkScopeGuard() noexcept
+    {
+        mFun();
+    }
+
+private:
+    std::function<void() noexcept> mFun;
+};
+
+} // namespace Akonadi
+
+#endif
