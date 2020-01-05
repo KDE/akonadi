384d6f1c4aee524b07f4eaf34949532e00b647d4
Daniel Vrátil <dvratil@kde.org>
2020-01-05 15:44:20 +0100

    Fix possible crash due to QStringBuilder auto type

--- src/server/search/searchmanager.cpp
+++ src/server/search/searchmanager.cpp
@@ -298,7 +298,7 @@ void SearchManager::updateSearchImpl(const Collection &collection)
     }
 
     // Query all plugins for search results
-    const auto id = "searchUpdate-" + QByteArray::number(QDateTime::currentDateTimeUtc().toTime_t());
+    const QByteArray id = "searchUpdate-" + QByteArray::number(QDateTime::currentDateTimeUtc().toTime_t());
     SearchRequest request(id, *this, mAgentSearchManager);
     request.setCollections(queryCollections);
     request.setMimeTypes(queryMimeTypes);
