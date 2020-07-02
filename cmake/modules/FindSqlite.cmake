09787f74b5a3f9c6d54d7239b1680e698ab7efab
Christophe Giboudeaux <christophe@krop.fr>
2020-07-02 13:35:40 +0000

    Convert license headers to SPDX format

--- cmake/modules/FindSqlite.cmake
+++ cmake/modules/FindSqlite.cmake
@@ -1,25 +1,3 @@
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products 
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # - Try to find Sqlite
 # Once done this will define
@@ -29,13 +7,12 @@
 #  SQLITE_LIBRARIES - Link these to use Sqlite
 #  SQLITE_MIN_VERSION - The minimum SQLite version
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# SPDX-FileCopyrightText: 2008 Gilles Caulier <caulier.gilles@gmail.com>
+# SPDX-FileCopyrightText: 2010 Christophe Giboudeaux <cgiboudeaux@gmail.com>
+# SPDX-FileCopyrightText: 2014 Daniel Vrátil <dvratil@redhat.com>
 #
-# Copyright (c) 2008, Gilles Caulier, <caulier.gilles@gmail.com>
-# Copyright (c) 2010, Christophe Giboudeaux, <cgiboudeaux@gmail.com>
-# Copyright (c) 2014, Daniel Vrátil <dvratil@redhat.com>
-
+# SPDX-License-Identifier: BSD-3-Clause
+#
 if(NOT SQLITE_MIN_VERSION)
   set(SQLITE_MIN_VERSION "3.6.16")
 endif(NOT SQLITE_MIN_VERSION)
