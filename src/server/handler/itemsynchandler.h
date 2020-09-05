b533142f3c58e1a458f36e7694821b98cb86fd0e
Daniel Vrátil <dvratil@kde.org>
2020-09-05 16:27:58 +0200

    Implement ItemSyncHandler
    This is a new handler for item syncing - internally it defers to
    ItemCreateHandler and ItemDeleteHandler. This allows client-side
    Itemsync to simply just create a bunch of jobs and let the server
    to take care of the rest - including optimal transaction handling
    and local listing.

---
+++ src/server/handler/itemsynchandler.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   SPDX-FileCopyrightText: 2020  Daniel Vrátil <dvratil@kde.org>         *
+ *                                                                         *
+ *   SPDX-License-Identifier: LGPL-2.0-or-later                            *
+ ***************************************************************************/
+
+#ifndef AKONADI_ITEMSYNCHANDLER_H_
+#define AKONADI_ITEMSYNCHANDLER_H_
+
+#include "handler.h"
+
+
+namespace Akonadi::Server
+{
+
+class ItemSyncer;
+class IncrementalItemSyncer;
+class FullItemSyncer;
+class ItemSyncHandler : public Handler
+{
+public:
+    explicit ItemSyncHandler(AkonadiServer &akonadi);
+
+    bool parseStream() override;
+
+    std::unique_ptr<ItemSyncer> createSyncer(ItemSyncHandler *handler, bool incremental) const;
+
+protected:
+    quint64 nextTag();
+
+    friend class ItemSyncer;
+    friend class IncrementalItemSyncer;
+    friend class FullItemSyncer;
+};
+
+} // namespace Akonadi::Server
+
+
+#endif
