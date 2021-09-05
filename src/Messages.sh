821c7769b36bc466fbd38c3df297ccf0f85f9ea6
Alexander Potashev <aspotashev@gmail.com>
2021-09-05 17:23:58 +0200

    Remove temporary file.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find -name "*.ui"` >> rc.cpp
 $XGETTEXT `find -name "*.cpp" -o -name "*.h" | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libakonadi5.pot
+rm -f rc.cpp
