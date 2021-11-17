26f0ea57c35e69f9fc419630cee038a99f8ec4cf
Laurent Montel <montel@kde.org>
2021-11-17 13:42:13 +0100

    GIT_SILENT: use <..> for include

--- src/widgets/agentactionmanager.cpp
+++ src/widgets/agentactionmanager.cpp
@@ -18,11 +18,11 @@
 #include <QAction>
 #include <QIcon>
 
-#include "ki18n_version.h"
 #include <QItemSelectionModel>
 #include <QPointer>
+#include <ki18n_version.h>
 #if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
-#include <klazylocalizedstring.h>
+#include <KLazyLocalizedString>
 #undef I18N_NOOP
 #define I18N_NOOP kli18n
 #endif
--- src/widgets/standardactionmanager.cpp
+++ src/widgets/standardactionmanager.cpp
@@ -49,9 +49,9 @@
 #include <QRegularExpression>
 #include <QTimer>
 
-#include "ki18n_version.h"
+#include <ki18n_version.h>
 #if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
-#include <klazylocalizedstring.h>
+#include <KLazyLocalizedString>
 #undef I18N_NOOP
 #define I18N_NOOP kli18n
 #define I18N_NOOP_EMPTY KLazyLocalizedString()
