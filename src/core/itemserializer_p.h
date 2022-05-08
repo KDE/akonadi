acca976fc19b2e802f2027e03c9fc868be5fd047
Laurent Montel <montel@kde.org>
2022-05-08 20:53:44 +0200

    Remove unimplemented constructor

--- src/core/itemserializer_p.h
+++ src/core/itemserializer_p.h
@@ -118,8 +118,6 @@ class StdStringItemSerializerPlugin : public QObject, public ItemSerializerPlugi
     Q_OBJECT
     Q_INTERFACES(Akonadi::ItemSerializerPlugin)
 public:
-    StdStringItemSerializerPlugin();
-
     bool deserialize(Item &item, const QByteArray &label, QIODevice &data, int version) override;
     void serialize(const Item &item, const QByteArray &label, QIODevice &data, int &version) override;
 };
