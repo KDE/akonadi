618ecb43398dea1f9b0b2f30c5b4c70ba6340521
Laurent Montel <montel@kde.org>
2022-06-23 07:33:47 +0200

    Add missing Q_REQUIRED_RESULT

--- src/agentbase/accountsintegration.h
+++ src/agentbase/accountsintegration.h
@@ -28,7 +28,7 @@ public:
     /**
      * Returns whether Accounts integration is enabled.
      */
-    bool isEnabled() const;
+    Q_REQUIRED_RESULT bool isEnabled() const;
 
     using AuthDataCallback = std::function<void(const QVariantMap &)>;
     using ErrorCallback = std::function<void(const QString &)>;
