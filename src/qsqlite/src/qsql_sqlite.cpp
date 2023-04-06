c3521046ad4be4b561f32624caa30cc911506166
Daniel Vrátil <dvratil@kde.org>
2023-04-06 09:31:55 +0200

    SQLite: enable multi-threaded mode
    By default SQLite runs in serialized mode. This flag allows database
    connections from multiple threads to operate more efficiently by not
    locking a lock when not needed.
    
    This has actually been the deault in the upstream SQLite driver since
    Qt 5.10.

--- src/qsqlite/src/qsql_sqlite.cpp
+++ src/qsqlite/src/qsql_sqlite.cpp
@@ -618,6 +618,8 @@ bool QSQLiteDriver::open(const QString &db, const QString &, const QString &, co
     if (openUriOption) {
         openMode |= SQLITE_OPEN_URI;
     }
+    // Use multi-threaded mode
+    openMode |= SQLITE_OPEN_NOMUTEX;
 
     sqlite3_enable_shared_cache(sharedCache);
 
