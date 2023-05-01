f277edb07f598243b7f6f768a828d9f986d2238a
Laurent Montel <montel@kde.org>
2023-05-01 16:10:07 +0200

    Allow to clear cache for folder + subfolder

---
+++ src/widgets/clearcachefoldersjob.cpp
@@ -0,0 +1,104 @@
+/*
+    This file is part of Akonadi
+
+    SPDX-FileCopyrightText: 2022-2023 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "clearcachefoldersjob.h"
+#include "akonadiwidgets_debug.h"
+#include "dbaccess.h"
+#include <KLocalizedString>
+#include <KMessageBox>
+#include <QSqlError>
+#include <QSqlQuery>
+
+using namespace Akonadi;
+ClearCacheFoldersJob::ClearCacheFoldersJob(const Akonadi::Collection &folder, QObject *parent)
+    : ClearCacheFoldersJob(Akonadi::Collection::List{folder}, parent)
+{
+}
+
+ClearCacheFoldersJob::ClearCacheFoldersJob(const Akonadi::Collection::List &folders, QObject *parent)
+    : QObject{parent}
+{
+    mCollections = folders;
+    mNumberJob = folders.length();
+    connect(this, &ClearCacheFoldersJob::clearNextFolder, this, &ClearCacheFoldersJob::slotClearNextFolder);
+}
+
+ClearCacheFoldersJob::~ClearCacheFoldersJob() = default;
+
+void ClearCacheFoldersJob::slotClearNextFolder()
+{
+    if (mNumberJob == -1) {
+        KMessageBox::information(mParentWidget, i18n("Collection cache cleared. You should restart Akonadi now."));
+        Q_EMIT clearCacheDone();
+        deleteLater();
+        return;
+    }
+    const auto akonadiId = mCollections.at(mNumberJob).id();
+    const auto ridCount = QStringLiteral("SELECT COUNT(*) FROM PimItemTable WHERE collectionId=%1 AND remoteId=''").arg(akonadiId);
+    QSqlQuery query(DbAccess::database());
+    if (!query.exec(ridCount)) {
+        qCWarning(AKONADIWIDGETS_LOG) << "Failed to execute query" << ridCount << ":" << query.lastError().text();
+        KMessageBox::error(mParentWidget, query.lastError().text());
+        deleteLater();
+        return;
+    }
+
+    query.next();
+    const int emptyRidCount = query.value(0).toInt();
+    if (emptyRidCount > 0) {
+        if (KMessageBox::warningContinueCancel(mParentWidget,
+                                               i18n("The collection '%1' contains %2 items without Remote ID. "
+                                                    "Those items were likely never uploaded to the destination server, "
+                                                    "so clearing this collection means that those <b>data will be lost</b>. "
+                                                    "Are you sure you want to proceed?",
+                                                    QString::number(akonadiId),
+                                                    QString::number(emptyRidCount)),
+                                               QStringLiteral("POSSIBLE DATA LOSS!"))
+            == KMessageBox::Cancel) {
+            deleteLater();
+            return;
+        }
+    }
+
+    const QString str = QStringLiteral("DELETE FROM PimItemTable WHERE collectionId=%1").arg(akonadiId);
+    qCWarning(AKONADIWIDGETS_LOG) << str;
+    query = QSqlQuery(str, DbAccess::database());
+    if (query.exec()) {
+        if (query.lastError().isValid()) {
+            qCWarning(AKONADIWIDGETS_LOG) << query.lastError();
+            KMessageBox::error(mParentWidget, query.lastError().text());
+        }
+    }
+    mNumberJob--;
+    Q_EMIT clearNextFolder();
+}
+
+void ClearCacheFoldersJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        // TODO emit signal
+        return;
+    }
+    mNumberJob--;
+    Q_EMIT clearNextFolder();
+}
+
+bool ClearCacheFoldersJob::canStart() const
+{
+    return !mCollections.isEmpty();
+}
+
+QWidget *ClearCacheFoldersJob::parentWidget() const
+{
+    return mParentWidget;
+}
+
+void ClearCacheFoldersJob::setParentWidget(QWidget *newParentWidget)
+{
+    mParentWidget = newParentWidget;
+}
---
+++ src/widgets/clearcachefoldersjob.h
@@ -0,0 +1,40 @@
+/*
+    This file is part of Akonadi
+
+    SPDX-FileCopyrightText: 2022-2023 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include "akonadiwidgets_export.h"
+#include <Akonadi/Collection>
+#include <QObject>
+namespace Akonadi
+{
+class AKONADIWIDGETS_EXPORT ClearCacheFoldersJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ClearCacheFoldersJob(const Akonadi::Collection &folder, QObject *parent = nullptr);
+    explicit ClearCacheFoldersJob(const Akonadi::Collection::List &folders, QObject *parent = nullptr);
+    ~ClearCacheFoldersJob() override;
+
+    void start();
+
+    Q_REQUIRED_RESULT bool canStart() const;
+
+    Q_REQUIRED_RESULT QWidget *parentWidget() const;
+    void setParentWidget(QWidget *newParentWidget);
+
+Q_SIGNALS:
+    void clearCacheDone();
+    void clearNextFolder();
+
+private:
+    void slotClearNextFolder();
+    Akonadi::Collection::List mCollections;
+    QWidget *mParentWidget = nullptr;
+    int mNumberJob = 0;
+};
+}
