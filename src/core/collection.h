ea93a0b582c93879577252e59861868f8e989bac
Laurent Montel <montel@kde.org>
2023-07-26 07:53:35 +0200

    The classes all have a Q_GADGET macro and thus have automatic metatype declaration.
    The explicit declaration macros only add costs to all places using the
    headers, where the metatype declaration will be only needed by code
    using the types in queued connections.
    
    See also end of section here: https://doc.qt.io/qt-6/qmetatype.html#Q_DECLARE_METATYPE

--- src/core/collection.h
+++ src/core/collection.h
@@ -602,7 +602,6 @@ inline bool Akonadi::Collection::hasAttribute() const
  */
 AKONADICORE_EXPORT QDebug operator<<(QDebug d, const Akonadi::Collection &collection);
 
-Q_DECLARE_METATYPE(Akonadi::Collection)
 Q_DECLARE_METATYPE(Akonadi::Collection::List)
 Q_DECLARE_OPERATORS_FOR_FLAGS(Akonadi::Collection::Rights)
 Q_DECLARE_TYPEINFO(Akonadi::Collection, Q_MOVABLE_TYPE);
