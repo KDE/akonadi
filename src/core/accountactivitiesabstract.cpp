eef680a97500bb94b77e1942d95bbf67dc8da7e5
Laurent Montel <montel@kde.org>
2024-05-21 08:52:37 +0200

    Add accountactivitiesabstract class

---
+++ src/core/accountactivitiesabstract.cpp
@@ -0,0 +1,15 @@
+/*
+    SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "accountactivitiesabstract.h"
+using namespace Akonadi;
+AccountActivitiesAbstract::AccountActivitiesAbstract(QObject *parent)
+    : QObject{parent}
+{
+}
+
+AccountActivitiesAbstract::~AccountActivitiesAbstract() = default;
+
+#include "moc_accountactivitiesabstract.cpp"
---
+++ src/core/accountactivitiesabstract.h
@@ -0,0 +1,28 @@
+/*
+    SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "akonadicore_export.h"
+#include <QObject>
+namespace Akonadi
+{
+class AKONADICORE_EXPORT AccountActivitiesAbstract : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AccountActivitiesAbstract(QObject *parent = nullptr);
+    ~AccountActivitiesAbstract() override;
+
+    [[nodiscard]] virtual bool filterAcceptsRow(const QStringList &activities) const = 0;
+
+    [[nodiscard]] virtual bool hasActivitySupport() const = 0;
+
+    [[nodiscard]] virtual QString currentActivity() const = 0;
+Q_SIGNALS:
+    void activitiesChanged();
+};
+}
