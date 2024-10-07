0076057480e636a8a1744f9b57e5cb270098832d
Laurent Montel <montel@kde.org>
2024-10-07 08:13:01 +0200

    We need a AgentInstanceFilterProxyModel specific which have activities support

---
+++ src/core/models/agentinstancefilterproxymodel.cpp
@@ -0,0 +1,176 @@
+/*
+    SPDX-FileCopyrightText: 2007 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "agentinstancefilterproxymodel.h"
+
+#include "accountactivitiesabstract.h"
+#include "agentinstancemodel.h"
+
+#include <QMimeDatabase>
+#include <QMimeType>
+#include <QStringList>
+
+using namespace Akonadi;
+/**
+ * @internal
+ */
+class Akonadi::AgentInstanceFilterProxyModelPrivate
+{
+public:
+    QStringList mimeTypes;
+    QStringList capabilities;
+    QStringList excludeCapabilities;
+    bool enablePlasmaActivities = false;
+    AccountActivitiesAbstract *accountActivitiesAbstract = nullptr;
+    [[nodiscard]] bool filterAcceptRegExp(const QModelIndex &index, const QRegularExpression &filterRegExpStr);
+};
+
+AgentInstanceFilterProxyModel::AgentInstanceFilterProxyModel(QObject *parent)
+    : QSortFilterProxyModel(parent)
+    , d(new AgentInstanceFilterProxyModelPrivate)
+{
+}
+
+AgentInstanceFilterProxyModel::~AgentInstanceFilterProxyModel() = default;
+
+void AgentInstanceFilterProxyModel::addMimeTypeFilter(const QString &mimeType)
+{
+    d->mimeTypes << mimeType;
+    invalidateFilter();
+}
+
+void AgentInstanceFilterProxyModel::addCapabilityFilter(const QString &capability)
+{
+    d->capabilities << capability;
+    invalidateFilter();
+}
+
+void AgentInstanceFilterProxyModel::excludeCapabilities(const QString &capability)
+{
+    d->excludeCapabilities << capability;
+    invalidateFilter();
+}
+
+bool AgentInstanceFilterProxyModel::enablePlasmaActivities() const
+{
+    return d->enablePlasmaActivities;
+}
+
+void AgentInstanceFilterProxyModel::setEnablePlasmaActivities(bool newEnablePlasmaActivities)
+{
+    d->enablePlasmaActivities = newEnablePlasmaActivities;
+    invalidateFilter();
+}
+
+void AgentInstanceFilterProxyModel::clearFilters()
+{
+    d->capabilities.clear();
+    d->mimeTypes.clear();
+    d->excludeCapabilities.clear();
+    invalidateFilter();
+}
+
+bool AgentInstanceFilterProxyModelPrivate::filterAcceptRegExp(const QModelIndex &index, const QRegularExpression &filterRegExpStr)
+{
+    // First see if the name matches a set regexp filter.
+    if (!filterRegExpStr.pattern().isEmpty()) {
+        return index.data(AgentInstanceModel::TypeIdentifierRole).toString().contains(filterRegExpStr) || index.data().toString().contains(filterRegExpStr);
+    }
+    return true;
+}
+
+bool AgentInstanceFilterProxyModel::filterAcceptsRow(int row, const QModelIndex & /*source_parent*/) const
+{
+    const QModelIndex index = sourceModel()->index(row, 0);
+
+    if (!d->mimeTypes.isEmpty()) {
+        QMimeDatabase mimeDb;
+        bool found = false;
+        const QStringList lst = index.data(AgentInstanceModel::MimeTypesRole).toStringList();
+        for (const QString &mimeType : lst) {
+            if (d->mimeTypes.contains(mimeType)) {
+                found = true;
+            } else {
+                const QMimeType mt = mimeDb.mimeTypeForName(mimeType);
+                if (mt.isValid()) {
+                    for (const QString &type : std::as_const(d->mimeTypes)) {
+                        if (mt.inherits(type)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        }
+    }
+
+    if (!d->capabilities.isEmpty()) {
+        bool found = false;
+        const QStringList lst = index.data(AgentInstanceModel::CapabilitiesRole).toStringList();
+        for (const QString &capability : lst) {
+            if (d->capabilities.contains(capability)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        }
+
+        if (!d->excludeCapabilities.isEmpty()) {
+            const QStringList lstCapabilities = index.data(AgentInstanceModel::CapabilitiesRole).toStringList();
+            for (const QString &capability : lstCapabilities) {
+                if (d->excludeCapabilities.contains(capability)) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+    }
+    const bool result = d->filterAcceptRegExp(index, filterRegularExpression());
+    if (result) {
+        if (d->accountActivitiesAbstract && d->enablePlasmaActivities) {
+            const bool enableActivities = index.data(AgentInstanceModel::ActivitiesEnabledRole).toBool();
+            if (enableActivities) {
+                const auto activities = index.data(AgentInstanceModel::ActivitiesRole).toStringList();
+                const bool result = d->accountActivitiesAbstract->filterAcceptsRow(activities);
+                // qDebug() << " result " << result << " identity name : " << sourceModel()->index(source_row,
+                // IdentityTreeModel::IdentityNameRole).data().toString();
+                return result;
+            }
+        }
+    }
+    return result;
+}
+
+AccountActivitiesAbstract *AgentInstanceFilterProxyModel::accountActivitiesAbstract() const
+{
+    return d->accountActivitiesAbstract;
+}
+
+void AgentInstanceFilterProxyModel::setAccountActivitiesAbstract(AccountActivitiesAbstract *abstract)
+{
+    if (d->accountActivitiesAbstract != abstract) {
+        d->accountActivitiesAbstract = abstract;
+        connect(d->accountActivitiesAbstract, &AccountActivitiesAbstract::activitiesChanged, this, &AgentInstanceFilterProxyModel::invalidateFilter);
+        invalidateFilter();
+    }
+}
+
+#include "moc_agentinstancefilterproxymodel.cpp"
---
+++ src/core/models/agentinstancefilterproxymodel.h
@@ -0,0 +1,95 @@
+/*
+    SPDX-FileCopyrightText: 2007 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "akonadicore_export.h"
+#include <QSortFilterProxyModel>
+
+#include <memory>
+
+namespace Akonadi
+{
+class AgentInstanceFilterProxyModelPrivate;
+class AccountActivitiesAbstract;
+/**
+ * @short A proxy model for filtering AgentType or AgentInstance
+ *
+ * This filter proxy model works on top of a AgentTypeModel or AgentInstanceModel
+ * and can be used to show only AgentType or AgentInstance objects
+ * which provide a given mime type or capability.
+ *
+ * @code
+ *
+ * // Show only running agent instances that provide contacts
+ * Akonadi::AgentInstanceModel *model = new Akonadi::AgentInstanceModel( this );
+ *
+ * Akonadi::AgentInstanceFilterProxyModel *proxy = new Akonadi::AgentInstanceFilterProxyModel( this );
+ * proxy->addMimeTypeFilter( "text/directory" );
+ *
+ * proxy->setSourceModel( model );
+ *
+ * QListView *view = new QListView( this );
+ * view->setModel( proxy );
+ *
+ * @endcode
+ *
+ * @author Volker Krause <vkrause@kde.org>
+ */
+class AKONADICORE_EXPORT AgentInstanceFilterProxyModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    /**
+     * Create a new agent filter proxy model.
+     * By default no filtering is done.
+     * @param parent parent object
+     */
+    explicit AgentInstanceFilterProxyModel(QObject *parent = nullptr);
+
+    /**
+     * Destroys the agent filter proxy model.
+     */
+    ~AgentInstanceFilterProxyModel() override;
+
+    /**
+     * Accept agents supporting @p mimeType.
+     */
+    void addMimeTypeFilter(const QString &mimeType);
+
+    /**
+     * Accept agents with the given @p capability.
+     */
+    void addCapabilityFilter(const QString &capability);
+
+    /**
+     * Clear the filters ( mimeTypes & capabilities ).
+     */
+    void clearFilters();
+
+    /**
+     * Excludes agents with the given @p capability.
+     * @param capability undesired agent capability
+     * @since 4.6
+     */
+    void excludeCapabilities(const QString &capability);
+
+    [[nodiscard]] bool enablePlasmaActivities() const;
+    void setEnablePlasmaActivities(bool newEnablePlasmaActivities);
+
+    [[nodiscard]] AccountActivitiesAbstract *accountActivitiesAbstract() const;
+    void setAccountActivitiesAbstract(AccountActivitiesAbstract *abstract);
+
+protected:
+    bool filterAcceptsRow(int row, const QModelIndex &parent) const override;
+
+private:
+    /// @cond PRIVATE
+    std::unique_ptr<AgentInstanceFilterProxyModelPrivate> const d;
+    /// @endcond
+};
+
+}
