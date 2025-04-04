KDE/akonadi

commit 616151f991096ffd3143ee749a70c1361646bca0

Jobs: when scope is UID-based, split it to multiple batches
This is more or less a workaround for known limitations of the SQL engines
we use. They each have some limitations when it comes to the number of bound
parameters or the overal size of the SQL query string. We can hit this limit
when querying multitude of singular items through a single job (e.g. the
Indexing agent will discover unindexed Items in a Collection and will attempt
to fetch them all with a single ItemFetchJob - potentially fetching tens of
thousands of Items, or user selecting and deleting a very large number of
emails from KMail). Even though the limits of SQL engines are 32k or
more, we intentionally set the transaction size to 10'000 items - this
means that its unlikely to be hit in common scenarios and also gives as
almost absolute certainty that we will not make hit the SQL limits,
ever.

While the best place to handle it would be in the database code, the way our
database abstraction work right now makes it super-hard to implement. It's
easier to instead handle this on the client side by splitting the
requested scope into multiple smaller batches and process each batch
with a separate FetchItemsCommand. This way we know for sure that the
SQL queries on the Server won't hit any limits and we make it absolutely
transparent to the user that we received their thousands of messages
through multiple fetches instead of one.

While in theory the same problem applies to Collections and Tags,
realistically, Items are the only entities that get affected, since
no-body has tens of thousands of Collections or Tags...

src/core/jobs/itemmodifyjob.cpp
@@ -22,6 +22,8 @@
 
 #include <QFile>
 
+#include <span>
+
 using namespace Akonadi;
 
 ItemModifyJobPrivate::ItemModifyJobPrivate(ItemModifyJob *parent)
@@ -97,6 +99,33 @@ void ItemModifyJobPrivate::setSilent(bool silent)
     mSilent = silent;
 }
 
+bool ItemModifyJobPrivate::nextBatch()
+{
+    Q_Q(ItemModifyJob);
+
+    if (mRemainingItems.empty()) {
+        return true;
+    }
+
+    Protocol::ModifyItemsCommandPtr command;
+    try {
+        command = fullCommand();
+    } catch (const Exception &e) {
+        q->setError(Job::Unknown);
+        q->setErrorText(QString::fromUtf8(e.what()));
+        q->emitResult();
+        return true;
+    }
+
+    if (command->modifiedParts() == Protocol::ModifyItemsCommand::None) {
+        q->emitResult();
+        return true;
+    }
+
+    sendCommand(command);
+    return false;
+}
+
 ItemModifyJob::ItemModifyJob(const Item &item, QObject *parent)
     : Job(new ItemModifyJobPrivate(this), parent)
 {
@@ -131,15 +160,13 @@ ItemModifyJob::ItemModifyJob(const Akonadi::Item::List &items, QObject *parent)
     }
 }
 
-ItemModifyJob::~ItemModifyJob()
-{
-}
+ItemModifyJob::~ItemModifyJob() = default;
 
 Protocol::ModifyItemsCommandPtr ItemModifyJobPrivate::fullCommand() const
 {
     auto cmd = Protocol::ModifyItemsCommandPtr::create();
 
-    const Akonadi::Item item = mItems.first();
+    const Akonadi::Item &item = mRemainingItems.front();
     for (int op : std::as_const(mOperations)) {
         switch (op) {
         case ItemModifyJobPrivate::RemoteId:
@@ -229,7 +256,11 @@ Protocol::ModifyItemsCommandPtr ItemModifyJobPrivate::fullCommand() const
         return cmd;
     }
 
-    cmd->setItems(ProtocolHelper::entitySetToScope(mItems));
+    const auto batchSize = qMin(MaxBatchSize, mRemainingItems.size());
+    const auto batch = mRemainingItems.subspan(0, batchSize);
+    mRemainingItems = mRemainingItems.subspan(batch.size());
+
+    cmd->setItems(ProtocolHelper::entitySetToScope(QList(batch.begin(), batch.end())));
     if (mRevCheck && item.revision() >= 0) {
         cmd->setOldRevision(item.revision());
     }
@@ -245,22 +276,8 @@ void ItemModifyJob::doStart()
 {
     Q_D(ItemModifyJob);
 
-    Protocol::ModifyItemsCommandPtr command;
-    try {
-        command = d->fullCommand();
-    } catch (const Exception &e) {
-        setError(Job::Unknown);
-        setErrorText(QString::fromUtf8(e.what()));
-        emitResult();
-        return;
-    }
-
-    if (command->modifiedParts() == Protocol::ModifyItemsCommand::None) {
-        emitResult();
-        return;
-    }
-
-    d->sendCommand(command);
+    d->mRemainingItems = std::span(d->mItems);
+    d->nextBatch();
 }
 
 bool ItemModifyJob::doHandleResponse(qint64 tag, const Protocol::CommandPtr &response)
@@ -331,6 +348,12 @@ bool ItemModifyJob::doHandleResponse(qint64 tag, const Protocol::CommandPtr &res
             // There will be more responses, either for other modified items,
             // or the final response with invalid ID, but with modification datetime
             return false;
+        }
+
+        if (!d->mRemainingItems.empty()) {
+            if (!d->nextBatch()) {
+                return false;
+            }
         }
 
         for (const Item &item : std::as_const(d->mItems)) {

autotests/libs/itemfetchtest.cpp
@@ -263,4 +263,56 @@ void ItemFetchTest::testAncestorRetrieval()
     QCOMPARE(c3, Collection::root());
 }
 
+void ItemFetchTest::testFetchBatching()
+{
+    static constexpr size_t ItemCount = 20'010; // batch size is 10000, so we should need two full and one partial batch
+
+    auto resolver = new CollectionPathResolver(QStringLiteral("res1/foo"), this);
+    AKVERIFYEXEC(resolver);
+    int colId = resolver->collection();
+
+    auto *itemCountJob = new ItemFetchJob(Collection(colId));
+    AKVERIFYEXEC(itemCountJob);
+    const auto origItemsCount = itemCountJob->items().size();
+
+    QList<Item> items;
+    items.reserve(ItemCount);
+    qDebug() << "Creating" << ItemCount << "items";
+    ItemCreateJob *job = nullptr;
+    for (size_t i = 0; i < ItemCount; ++i) {
+        Item item;
+        item.setMimeType(QStringLiteral("application/octet-stream"));
+        item.setPayload<QByteArray>("body data");
+        job = new ItemCreateJob(item, Collection(colId), this);
+        connect(job, &ItemCreateJob::result, this, [job, &items]() {
+            QCOMPARE(job->error(), KJob::NoError);
+            items.push_back(job->item());
+            if (items.size() % 1000 == 0) {
+                qDebug() << "Created" << items.size() << "items";
+            }
+        });
+    }
+    // Wait for the last one synchronously
+    AKVERIFYEXEC(job);
+
+    // Now try to fetch all the 20'010 items by explicitly listing them all
+    qDebug() << "Fetching" << items.size() << "items";
+    auto *fetchJob = new ItemFetchJob(items, this);
+    AKVERIFYEXEC(fetchJob);
+    qDebug() << "Done";
+
+    QCOMPARE(fetchJob->items().size(), ItemCount);
+
+    // And now try to delete all of them by listing them explicitly
+    qDebug() << "Deleting" << items.size() << "items";
+    auto *deleteJob = new ItemDeleteJob(items, this);
+    AKVERIFYEXEC(deleteJob);
+    qDebug() << "Done";
+
+    // Finally, confirm that all the items are gone
+    itemCountJob = new ItemFetchJob(Collection(colId));
+    AKVERIFYEXEC(itemCountJob);
+    QCOMPARE(origItemsCount, itemCountJob->items().size());
+}
+
 #include "moc_itemfetchtest.cpp"

autotests/libs/itemfetchtest.h
@@ -20,4 +20,5 @@ private Q_SLOTS:
     void testMultipartFetch();
     void testRidFetch();
     void testAncestorRetrieval();
+    void testFetchBatching();
 };

autotests/libs/itemmovetest.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "collection.h"
+#include "collectioncreatejob.h"
 #include "collectionfetchscope.h"
 #include "control.h"
 #include "itemcreatejob.h"
@@ -167,6 +168,50 @@ private Q_SLOTS:
         ntfItem = ntfItems.at(0);
         QCOMPARE(ntfItem.id(), item.id());
         QCOMPARE(ntfItem.flags(), item.flags());
+    }
+
+    void testMoveLargeBatch()
+    {
+        static constexpr size_t ItemCount = 20'010; // batch size is 10,000, so we should need two full and one partial batch
+
+        Collection col(AkonadiTest::collectionIdFromPath(QStringLiteral("res1/foo")));
+        QVERIFY(col.isValid());
+
+        // Create a few thousand items to force batching
+        QList<Item> items;
+        items.reserve(ItemCount);
+        qDebug() << "Creating" << ItemCount << "items";
+        ItemCreateJob *job = nullptr;
+        for (size_t i = 0; i < ItemCount; ++i) {
+            Item item;
+            item.setMimeType(QStringLiteral("application/octet-stream"));
+            item.setPayload<QByteArray>("body data");
+            job = new ItemCreateJob(item, col, this);
+            connect(job, &ItemCreateJob::result, this, [job, &items]() {
+                QCOMPARE(job->error(), KJob::NoError);
+                items.push_back(job->item());
+                if (items.size() % 1000 == 0) {
+                    qDebug() << "Created" << items.size() << "items";
+                }
+            });
+        }
+        // Wait for the last one synchronously
+        AKVERIFYEXEC(job);
+
+        Collection dest;
+        dest.setName(QStringLiteral("bar2"));
+        dest.setParentCollection(col);
+        auto newCol = new CollectionCreateJob(dest, this);
+        AKVERIFYEXEC(newCol);
+        dest = newCol->collection();
+
+        auto move = new ItemMoveJob(items, dest, this);
+        AKVERIFYEXEC(move);
+
+        auto fetch = new ItemFetchJob(dest, this);
+        AKVERIFYEXEC(fetch);
+
+        QCOMPARE(fetch->items().count(), items.count());
     }
 };
 

autotests/libs/itemstoretest.cpp
@@ -414,6 +414,58 @@ void ItemStoreTest::itemModifyJobShouldOnlySendModifiedAttributes()
     }
 }
 
+void ItemStoreTest::testModifyLargeBatch()
+{
+    static constexpr size_t ItemCount = 20'010; // batch size is 10,000, so we should need two full and one partial batch
+
+    auto resolver = new CollectionPathResolver(QStringLiteral("res1"), this);
+    AKVERIFYEXEC(resolver);
+    int colId = resolver->collection();
+
+    // Create a few thousand items to force batching
+    QList<Item> items;
+    items.reserve(ItemCount);
+    qDebug() << "Creating" << ItemCount << "items";
+    ItemCreateJob *job = nullptr;
+    for (size_t i = 0; i < ItemCount; ++i) {
+        Item item;
+        item.setMimeType(QStringLiteral("application/octet-stream"));
+        item.setPayload<QByteArray>("body data");
+        job = new ItemCreateJob(item, Collection(colId), this);
+        connect(job, &ItemCreateJob::result, this, [job, &items]() {
+            QCOMPARE(job->error(), KJob::NoError);
+            items.push_back(job->item());
+            if (items.size() % 1000 == 0) {
+                qDebug() << "Created" << items.size() << "items";
+            }
+        });
+    }
+    // Wait for the last one synchronously
+    AKVERIFYEXEC(job);
+
+    // Now mark all items as "read"
+    QCOMPARE(items.size(), ItemCount);
+    std::for_each(items.begin(), items.end(), [](Item &item) {
+        item.setFlag(Item::Flag("\\SEEN"));
+    });
+
+    qDebug() << "Updating items";
+    // And update them all
+    auto *modifyJob = new ItemModifyJob(items, this);
+    AKVERIFYEXEC(modifyJob);
+    qDebug() << "Done";
+
+    // Fetch everything again, verify they all have the flag
+    qDebug() << "Verifying flags";
+    auto *fetchJob = new ItemFetchJob(items, this);
+    AKVERIFYEXEC(fetchJob);
+    const auto fetchedItems = fetchJob->items();
+    QCOMPARE(fetchedItems.size(), ItemCount);
+    QVERIFY(std::all_of(fetchedItems.begin(), fetchedItems.end(), [](const Item &item) {
+        return item.hasFlag("\\SEEN");
+    }));
+}
+
 class ParallelJobsRunner
 {
 public:

autotests/libs/itemstoretest.h
@@ -26,4 +26,5 @@ private Q_SLOTS:
     void testRemoteIdRace();
     void itemModifyJobShouldOnlySendModifiedAttributes();
     void testParallelJobsAddingAttributes();
+    void testModifyLargeBatch();
 };

src/core/jobs/itemdeletejob.cpp
@@ -11,14 +11,39 @@
 #include "private/protocol_p.h"
 #include "protocolhelper_p.h"
 
+#include <span>
+
 using namespace Akonadi;
 
 class Akonadi::ItemDeleteJobPrivate : public JobPrivate
 {
+    static constexpr std::size_t MaxBatchSize = 10'000UL;
+
 public:
     explicit ItemDeleteJobPrivate(ItemDeleteJob *parent)
         : JobPrivate(parent)
     {
+    }
+
+    bool requestBatch()
+    {
+        if (!mItems.empty()) { // item-based removal
+            const auto batchSize = qMin(MaxBatchSize, mRemainingItems.size());
+            if (batchSize == 0) {
+                return true;
+            }
+
+            const auto batch = mRemainingItems.subspan(0, batchSize);
+            mRemainingItems = mRemainingItems.subspan(batch.size());
+            const auto batchItems = QList(batch.begin(), batch.end());
+
+            sendCommand(Protocol::DeleteItemsCommandPtr::create(ProtocolHelper::entitySetToScope(batchItems),
+                                                                ProtocolHelper::commandContextToProtocol(mCollection, mCurrentTag, batchItems)));
+        } else { // collection- or tag-based removal
+            sendCommand(Protocol::DeleteItemsCommandPtr::create(Scope(), ProtocolHelper::commandContextToProtocol(mCollection, mCurrentTag, {})));
+        }
+
+        return false;
     }
 
     Q_DECLARE_PUBLIC(ItemDeleteJob)
@@ -27,6 +52,7 @@ public:
     Item::List mItems;
     Collection mCollection;
     Tag mCurrentTag;
+    std::span<Item> mRemainingItems;
 };
 
 QString Akonadi::ItemDeleteJobPrivate::jobDebuggingString() const
@@ -51,6 +77,7 @@ ItemDeleteJob::ItemDeleteJob(const Item &item, QObject *parent)
     Q_D(ItemDeleteJob);
 
     d->mItems << item;
+    d->mRemainingItems = std::span(d->mItems);
 }
 
 ItemDeleteJob::ItemDeleteJob(const Item::List &items, QObject *parent)
@@ -59,6 +86,7 @@ ItemDeleteJob::ItemDeleteJob(const Item::List &items, QObject *parent)
     Q_D(ItemDeleteJob);
 
     d->mItems = items;
+    d->mRemainingItems = std::span(d->mItems);
 }
 
 ItemDeleteJob::ItemDeleteJob(const Collection &collection, QObject *parent)
@@ -77,9 +105,7 @@ ItemDeleteJob::ItemDeleteJob(const Tag &tag, QObject *parent)
     d->mCurrentTag = tag;
 }
 
-ItemDeleteJob::~ItemDeleteJob()
-{
-}
+ItemDeleteJob::~ItemDeleteJob() = default;
 
 Item::List ItemDeleteJob::deletedItems() const
 {
@@ -93,8 +119,7 @@ void ItemDeleteJob::doStart()
     Q_D(ItemDeleteJob);
 
     try {
-        d->sendCommand(Protocol::DeleteItemsCommandPtr::create(d->mItems.isEmpty() ? Scope() : ProtocolHelper::entitySetToScope(d->mItems),
-                                                               ProtocolHelper::commandContextToProtocol(d->mCollection, d->mCurrentTag, d->mItems)));
+        d->requestBatch();
     } catch (const Akonadi::Exception &e) {
         setError(Job::Unknown);
         setErrorText(QString::fromUtf8(e.what()));
@@ -105,8 +130,14 @@ void ItemDeleteJob::doStart()
 
 bool ItemDeleteJob::doHandleResponse(qint64 tag, const Protocol::CommandPtr &response)
 {
+    Q_D(ItemDeleteJob);
+
     if (!response->isResponse() || response->type() != Protocol::Command::DeleteItems) {
         return Job::doHandleResponse(tag, response);
+    }
+
+    if (!d->mRemainingItems.empty()) {
+        return d->requestBatch();
     }
 
     return true;

src/core/jobs/itemfetchjob.cpp
@@ -6,6 +6,7 @@
 
 #include "itemfetchjob.h"
 
+#include "akonadicore_debug.h"
 #include "attributefactory.h"
 #include "collection.h"
 #include "itemfetchscope.h"
@@ -17,10 +18,14 @@
 
 #include <QTimer>
 
+#include <span>
+
 using namespace Akonadi;
 
 class Akonadi::ItemFetchJobPrivate : public JobPrivate
 {
+    static constexpr std::size_t MaxBatchSize = 10'000UL;
+
 public:
     explicit ItemFetchJobPrivate(ItemFetchJob *parent)
         : JobPrivate(parent)
@@ -41,6 +46,7 @@ public:
             timeout();
         });
     }
+
     void aboutToFinish() override
     {
         timeout();
@@ -57,6 +63,42 @@ public:
             }
             mPendingItems.clear();
         }
+    }
+
+    bool requestBatch()
+    {
+        if (!mRequestedItems.empty()) {
+            // If there are more items to fetch, but we already received the LIMIT number of items,
+            // we are technically done...
+            if (mItemsLimit.limit() > -1 && mCount >= mItemsLimit.limit()) {
+                return true;
+            }
+
+            const auto batchSize = qMin(MaxBatchSize, mRemainingItems.size());
+            if (batchSize == 0) {
+                // If we would fetch an empty batch, we are done.
+                return true;
+            }
+
+            const auto batch = mRemainingItems.subspan(0, batchSize);
+            mRemainingItems = mRemainingItems.subspan(batch.size());
+            const auto batchItems = QList(batch.begin(), batch.end());
+            sendCommand(Protocol::FetchItemsCommandPtr::create(ProtocolHelper::entitySetToScope(batchItems),
+                                                               ProtocolHelper::commandContextToProtocol(mCollection, mCurrentTag, batchItems),
+                                                               ProtocolHelper::itemFetchScopeToProtocol(mFetchScope),
+                                                               ProtocolHelper::tagFetchScopeToProtocol(mFetchScope.tagFetchScope()),
+                                                               mItemsLimit));
+
+        } else { // collection- or tag-based fetch (unbatched)
+            Q_ASSERT(mCount == 0); // can only be called once
+            sendCommand(Protocol::FetchItemsCommandPtr::create(Scope(),
+                                                               ProtocolHelper::commandContextToProtocol(mCollection, mCurrentTag, mRequestedItems),
+                                                               ProtocolHelper::itemFetchScopeToProtocol(mFetchScope),
+                                                               ProtocolHelper::tagFetchScopeToProtocol(mFetchScope.tagFetchScope()),
+                                                               mItemsLimit));
+        }
+
+        return false;
     }
 
     QString jobDebuggingString() const override
@@ -105,6 +147,7 @@ public:
     ItemFetchJob::DeliveryOptions mDeliveryOptions = ItemFetchJob::Default;
     int mCount = 0;
     Protocol::FetchLimit mItemsLimit;
+    std::span<Item> mRemainingItems;
 };
 
 ItemFetchJob::ItemFetchJob(const Collection &collection, QObject *parent)
@@ -163,11 +206,8 @@ void ItemFetchJob::doStart()
     Q_D(ItemFetchJob);
 
     try {
-        d->sendCommand(Protocol::FetchItemsCommandPtr::create(d->mRequestedItems.isEmpty() ? Scope() : ProtocolHelper::entitySetToScope(d->mRequestedItems),
-                                                              ProtocolHelper::commandContextToProtocol(d->mCollection, d->mCurrentTag, d->mRequestedItems),
-                                                              ProtocolHelper::itemFetchScopeToProtocol(d->mFetchScope),
-                                                              ProtocolHelper::tagFetchScopeToProtocol(d->mFetchScope.tagFetchScope()),
-                                                              d->mItemsLimit));
+        d->mRemainingItems = std::span(d->mRequestedItems.begin(), d->mRequestedItems.size());
+        d->requestBatch();
     } catch (const Akonadi::Exception &e) {
         setError(Job::Unknown);
         setErrorText(QString::fromUtf8(e.what()));
@@ -187,7 +227,10 @@ bool ItemFetchJob::doHandleResponse(qint64 tag, const Protocol::CommandPtr &resp
     const auto &resp = Protocol::cmdCast<Protocol::FetchItemsResponse>(response);
     // Invalid ID marks the last part of the response
     if (resp.id() < 0) {
-        return true;
+        if (d->mRemainingItems.empty()) {
+            return true;
+        }
+        return d->requestBatch();
     }
 
     const Item item = ProtocolHelper::parseItemFetchResult(resp, nullptr, d->mValuePool);

commit b28a8bdf7d4144a2b5f281b77a81ba06a34ea41b

Fix regression in ItemModifyJob request debugging
After introduction of batched job processing all resources using
ItemModifyJob would crash while Akonadi Console was running and
monitoring requests:

  #0  0x00007fffe8697cfc in ??? () at /usr/lib64/libc.so.6
  #1  0x00007fffe8640c26 in raise () at /usr/lib64/libc.so.6
  #2  0x00007fffe86288fa in abort () at /usr/lib64/libc.so.6
  #3  0x00007fffe8ad7daf in std::__glibcxx_assert_fail(char const*, int, char const*, char const*) () at /usr/lib/gcc/x86_64-pc-linux-gnu/14/libstdc++.so.6
  #4  0x00007ffff7a51d9f in std::span<Akonadi::Item, 18446744073709551615ul>::front (this=0x555556da6c60) at /usr/lib/gcc/x86_64-pc-linux-gnu/14/include/g++-v14/span:270
  #5  0x00007ffff7a4e89b in Akonadi::ItemModifyJobPrivate::fullCommand (this=0x555556da6bf0) at /akonadi/src/core/jobs/itemmodifyjob.cpp:169
  #6  0x00007ffff7a4e005 in Akonadi::ItemModifyJobPrivate::jobDebuggingString (this=0x555556da6bf0) /akonadi/src/core/jobs/itemmodifyjob.cpp:91
  #7  0x00007ffff7a5a3fe in Akonadi::JobPrivate::signalCreationToJobTracker (this=0x555556da6bf0) at /akonadi/src/core/jobs/job.cpp:140

The reason for the crash is use of ItemModifyJobPrivate::fullCommand()
without initializing the newly added member mRemainingItems. This
member is only initialized when regular item processing is done by
calling doStart().

Fix this by making sure that mRemainingItems is also initialized from
jobDebuggingString().

Note that this relies upon the fact that jobDebuggingString() is never
called during regular item processing, as it would effectively reset
the batch processing state.

Fixes: 616151f9 (Jobs: when scope is UID-based, split it to multiple batches)
Signed-off-by: Krzysztof Nowicki <krissn@op.pl>

src/core/jobs/itemmodifyjob.cpp
@@ -88,6 +88,7 @@ void ItemModifyJobPrivate::doUpdateItemRevision(Akonadi::Item::Id itemId, int ol
 QString ItemModifyJobPrivate::jobDebuggingString() const
 {
     try {
+        mRemainingItems = std::span(mItems);
         return Protocol::debugString(fullCommand());
     } catch (const Exception &e) {
         return QString::fromUtf8(e.what());