d14f9f5219b05b21736bff92933bf6abe0672f31
Carl Schwan <carl@carlschwan.eu>
2025-06-17 14:43:37 +0200

    firstrunner: Don't link to QtWidgets

--- autotests/libs/firstrunner.cpp
+++ autotests/libs/firstrunner.cpp
@@ -7,12 +7,12 @@
 #include "firstrun_p.h"
 
 #include <KAboutData>
-#include <QApplication>
 #include <QCommandLineParser>
+#include <QCoreApplication>
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
     KAboutData aboutData(QStringLiteral("akonadi-firstrun"), QStringLiteral("Test akonadi-firstrun"), QStringLiteral("0.10"));
     KAboutData::setApplicationData(aboutData);
@@ -23,6 +23,6 @@ int main(int argc, char **argv)
     aboutData.processCommandLine(&parser);
 
     auto f = new Akonadi::Firstrun();
-    QObject::connect(f, &Akonadi::Firstrun::destroyed, &app, &QApplication::quit);
+    QObject::connect(f, &Akonadi::Firstrun::destroyed, &app, &QCoreApplication::quit);
     app.exec();
 }
