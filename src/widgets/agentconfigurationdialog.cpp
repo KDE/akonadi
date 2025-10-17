f258a2f39a0d0421017e99e635f7add6091d3b49
Laurent Montel <montel@kde.org>
2025-10-17 08:37:02 +0200

    We depend against 6.17

--- src/widgets/agentconfigurationdialog.cpp
+++ src/widgets/agentconfigurationdialog.cpp
@@ -75,12 +75,8 @@ AgentConfigurationDialog::AgentConfigurationDialog(const AgentInstance &instance
 
     if (auto plugin = d->widget->d->plugin) {
         if (auto aboutData = plugin->aboutData()) {
-#if KXMLGUI_VERSION >= QT_VERSION_CHECK(6, 9, 0)
             auto helpMenu = new KHelpMenu(this, *aboutData);
             helpMenu->setShowWhatsThis(true);
-#else
-            auto helpMenu = new KHelpMenu(this, *aboutData, true);
-#endif
             helpMenu->action(KHelpMenu::menuDonate);
             // Initialize menu
             QMenu *menu = helpMenu->menu();
